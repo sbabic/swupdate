a4c94e334c4bdb0b95b8ea324a413abade93ed03
Romain Naour <romain.naour@openwide.fr>
2014-09-06 23:44:38 +0200

    lsqlite3: backport support for lua 5.2
    lua 5.2 is needed to build core/handler.c when lua support is enabled.
    
    The build fail if lua 5.1 is used:
    error: 'LUA_OK' undeclared (first use in this function)
    warning: implicit declaration of function 'luaL_newlib'
    warning: implicit declaration of function 'luaL_requiref'
    
    But the local copy of lsqlite3 needs lua 5.1 to build:
    warning: implicit declaration of function 'lua_strlen'
    warning: implicit declaration of function 'luaL_openlib'
    warning: implicit declaration of function 'luaL_register'
    undefined reference to `lua_strlen'
    undefined reference to `luaL_register'
    
    Backport lua 5.2 support from upstream commit
    [01d436858c] "working toward Lua 5.2 compatibility"
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>

--- mongoose/lsqlite3.c
+++ mongoose/lsqlite3.c
@@ -34,6 +34,19 @@
 #include "lua.h"
 #include "lauxlib.h"
 
+#if LUA_VERSION_NUM > 501
+//
+// Lua 5.2
+//
+#define lua_strlen lua_rawlen
+// luaL_typerror always used with arg at ndx == NULL
+#define luaL_typerror(L,ndx,str) luaL_error(L,"bad argument %d (%s expected, got nil)",ndx,str)
+// luaL_register used once, so below expansion is OK for this case
+#define luaL_register(L,name,reg) lua_newtable(L);luaL_setfuncs(L,reg,0)
+// luaL_openlib always used with name == NULL
+#define luaL_openlib(L,name,reg,nup) luaL_setfuncs(L,reg,nup)
+#endif
+
 #include "sqlite3.h"
 
 /* compile time features */
