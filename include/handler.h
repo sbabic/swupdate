3088d3082a0917df65546148f549d1831bbedb0a
Stefano Babic <sbabic@denx.de>
2015-03-25 12:12:56 +0100

    Drop reference to file CREDITS from headers
    Headers contain a reference to a CREDITS file
    that does not exist.
    
    The history with people contibuting to the project
    is maintained by git, no additional file is needed.
    
    Signed-off-by: Stefano Babic <sbabic@denx.de>
    Reported-by: Mario Domenech Goulart <mario@ossystems.com.br>

--- include/handler.h
+++ include/handler.h
@@ -3,9 +3,6 @@
  * Stefano Babic, DENX Software Engineering, sbabic@denx.de.
  * 	on behalf of ifm electronic GmbH
  *
- * See file CREDITS for list of people who contributed to this
- * project.
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
--- include/util.h
+++ include/util.h
@@ -2,9 +2,6 @@
  * (C) Copyright 2012-2013
  * Stefano Babic, DENX Software Engineering, sbabic@denx.de.
  *
- * See file CREDITS for list of people who contributed to this
- * project.
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
