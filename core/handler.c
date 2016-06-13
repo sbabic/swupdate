sbabic/swupdate

commit 4ee187718e706aa1358485fce7438e4ae1310cbb

Enhance output when searching for LUA handlers
The LUA interpreter shows an error if support for
handlers written in LUA is activated and the file
swupdate_handlers.lua is not found. This is not an error,
simply no additional handlers are registered into the
system. Try to explain this, and output that no
LUA handlers are found.

Signed-off-by: Stefano Babic <sbabic@denx.de>

core/handler.c
@@ -182,28 +182,35 @@ static int luaopen_swupdate(lua_State *L) {
 	return 1;
 }
 
-void lua_handlers_init(void)
+int lua_handlers_init(void)
 {
+	int ret = -1;
+
 	gL = NULL;
 	gL = luaL_newstate();
 	if (gL) {
+		printf("Searching for custom LUA handlers :");
 		/* load standard libraries */
 		luaL_openlibs(gL);
 		luaL_requiref( gL, "swupdate", luaopen_swupdate, 1 );
 		/* try to load lua handlers for the swupdate system */
-		if(luaL_dostring(gL,"require (\"swupdate_handlers\")") != 0)
+		ret = luaL_dostring(gL,"require (\"swupdate_handlers\")");
+		if(ret != 0)
 		{
 			puts(lua_tostring(gL, -1));
-		}
+		} else
+			printf(" OK\n");
 	} else	{
 		printf ("Unable to register Lua context for callbacks\n");
 	}
+
+	return ret;
 }
 #else
-void lua_handlers_init(void) {}
+int lua_handlers_init(void) {0}
 #endif
 
-int register_handler(const char *desc, 
+int register_handler(const char *desc,
 		handler installer, void *data)
 {
 

core/swupdate.c
@@ -534,7 +534,9 @@ int main(int argc, char **argv)
 		}
 	}
 
-	lua_handlers_init();
+	if (lua_handlers_init())
+		printf("Custom handlers not found, no error, skipping...\n\n");
+
 	if(!get_hw_revision(&swcfg.hw))
 		printf("Running on %s Revision %s\n", swcfg.hw.boardname, swcfg.hw.revision);
 

include/lua_util.h
@@ -34,6 +34,6 @@ int run_lua_script(char *script, char *function, char *parms);
 void image2table(lua_State* L, struct img_type *img);
 #endif
 
-void lua_handlers_init(void);
+int lua_handlers_init(void);
 
 #endif

commit 1e9fc1e7aed34e48c04e0ec437a4520b66637e1b

handler: fix build error
The build is broken if CONFIG_HANDLER_IN_LUA is not defined.
Fix error introduced by commit 4ee18771.

Signed-off-by: Anatolij Gustschin <agust@denx.de>

core/handler.c
@@ -207,7 +207,7 @@ int lua_handlers_init(void)
 	return ret;
 }
 #else
-int lua_handlers_init(void) {0}
+int lua_handlers_init(void) {return 0;}
 #endif
 
 int register_handler(const char *desc,