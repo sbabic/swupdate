ad62a63d87e8c2f61ccfb56db03e73aa0f6bdf6e
Stefano Babic <sbabic@denx.de>
2017-02-28 10:33:48 +0100

    doc: using read the docs theme
    Signed-off-by: Stefano Babic <sbabic@denx.de>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -100,7 +100,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = u'default'
+#html_theme = u'default'
+html_theme = u'sphinx_rtd_theme'
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
