sbabic/swupdate

commit aef3186196da458cfdb0b8dc40ee423c67ca7be7

Fix use of symlinks to MTD partitions
Before detecting of MTD partition number, realpath() will resolve
specified MTD partition path. This enables update to reference MTD
partitions by alternate names, for example symbolic links created
by udev.

Signed-off-by: Magnus Olsson <magnus@minimum.se>

corelib/mtd-interface.c
@@ -51,13 +51,21 @@ void mtd_set_ubiblacklist(char *mtdlist)
 int get_mtd_from_device(char *s) {
 	int ret;
 	int mtdnum;
+	char *real_s;
 
 	if (!s)
 		return -1;
 
-	ret = sscanf(s, "mtd%d", &mtdnum);
+	real_s = realpath(s, NULL);
+	if (real_s == NULL)
+		return -1;
+
+	TRACE("mtd name [%s] resolved to [%s]\n", s, real_s);
+	ret = sscanf(real_s, "mtd%d", &mtdnum);
 	if (ret <= 0)
-		ret = sscanf(s, "/dev/mtd%d", &mtdnum);
+		ret = sscanf(real_s, "/dev/mtd%d", &mtdnum);
+
+	free (real_s);
 
 	if (ret <= 0)
 		return -1;

commit dbcbfffa2e31976e21ee67f9a717bb54cb32a5a4

Restore the documented behavior of device tag
Partition naming like "mtd4" broken by aef3186196 should
work again

Signed-off-by: Alexander Kuzmich <alku@hms.se>
Acked-by: Stefano Babic < sbabic@denx.de>

corelib/mtd-interface.c
@@ -26,6 +26,7 @@
 #include <sys/mount.h>
 #include <unistd.h>
 #include <errno.h>
+#include <limits.h>
 #include "bsdqueue.h"
 #include "util.h"
 #include "flash.h"
@@ -57,8 +58,17 @@ int get_mtd_from_device(char *s) {
 		return -1;
 
 	real_s = realpath(s, NULL);
-	if (real_s == NULL)
-		return -1;
+	if (real_s == NULL) {
+		char tmp_s[PATH_MAX] = {0};
+
+		if (! strncmp(s, "/dev/", 5))
+			return -1;
+
+		snprintf(tmp_s, sizeof(tmp_s), "/dev/%s", s);
+		real_s = realpath(tmp_s, NULL);
+		if (real_s == NULL)
+			return -1;
+	}
 
 	TRACE("mtd name [%s] resolved to [%s]\n", s, real_s);
 	ret = sscanf(real_s, "mtd%d", &mtdnum);