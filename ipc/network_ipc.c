c9998d6dfaadc4bbfda933677ee66722a8677b61
Stefano Babic <sbabic@denx.de>
2017-04-27 12:56:15 +0200

    LICENSE: relicensing the client library under LGPL
    SWUpdate can be driven from an external software via the control socket.
    The client library is thought for this purpose and it makes sense that
    only this part of the software (the library itself and the required
    header) are licensed under Less General Public License (LGPL).
    Commercial software can make use of the library without becoming GPL.
    
    Signed-off-by: Stefano Babic <sbabic@denx.de>

--- ipc/network_ipc.c
+++ ipc/network_ipc.c
@@ -1,22 +1,16 @@
 /*
- * (C) Copyright 2008-2013
+ * (C) Copyright 2008-2017
  * Stefano Babic, DENX Software Engineering, sbabic@denx.de.
  * 	on behalf of ifm electronic GmbH
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
- * GNU General Public License for more details.
+ * You should have received a copy of the Less General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
  */
 
 #include <stdio.h>
