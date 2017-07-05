a9e4bb0241a3c6119764a7124e766529a8da0c05
Christian Storm <christian.storm@siemens.com>
2017-07-05 15:07:32 +0200

    Lua: rework stackdump()
    LUAstackDump() is reworked
    * to use SWUpdate's notification system and
    * to be more informative at TRACELEVEL.
    
    Signed-off-by: Christian Storm <christian.storm@siemens.com>

--- corelib/lua_interface.c
+++ corelib/lua_interface.c
@@ -51,32 +51,62 @@
 static int l_register_handler( lua_State *L );
 #endif
 
-void LUAstackDump (lua_State *L) {
-	int i;
+void LUAstackDump(lua_State *L)
+{
 	int top = lua_gettop(L);
-	for (i = 1; i <= top; i++) { /* repeat for each level */
+	for (int i = 1; i <= top; i++) {
 		int t = lua_type(L, i);
 		switch (t) {
-			case LUA_TSTRING: { /* strings */
-						  printf("’%s’", lua_tostring(L, i));
-						  break;
-					  }
-			case LUA_TBOOLEAN: { /* booleans */
-						   printf(lua_toboolean(L, i) ? "true" : "false");
-						   break;
-					   }
-			case LUA_TNUMBER: { /* numbers */
-						  printf("%g", lua_tonumber(L, i));
-						  break;
-					  }
-			default: { /* other values */
-					 printf("%s", lua_typename(L, t));
-					 break;
-				 }
+			case LUA_TSTRING: {
+				TRACE("(%d) [string] %s", i, lua_tostring(L, i));
+				break;
+			}
+			case LUA_TBOOLEAN: {
+				TRACE("(%d) [bool  ] %s", i, (lua_toboolean(L, i) ? "true" : "false"));
+				break;
+			}
+			case LUA_TNUMBER: {
+				TRACE("(%d) [number] %g", i, lua_tonumber(L, i));
+				break;
+			}
+			case LUA_TTABLE: {
+				lua_pushvalue(L, -1);
+				lua_pushnil(L);
+				/* Stack: nil, table */
+				while (lua_next(L, -2)) {
+					/* Stack: value, key, table */
+					lua_pushvalue(L, -2);
+					/* Stack: key, value, key, table */
+					switch(lua_type(L, -2)) {
+						case LUA_TSTRING:
+						case LUA_TNUMBER:
+							TRACE("(%d) [table ] %s = %s", i,
+								lua_tostring(L, -1),
+								lua_tostring(L, -2));
+							break;
+						case LUA_TBOOLEAN:
+							TRACE("(%d) [table ] %s = %s", i,
+								lua_tostring(L, -1),
+								(lua_toboolean(L, -2) ? "true" : "false"));
+							break;
+						default:
+							TRACE("(%d) [table ] %s = <unparsed type>", i,
+								lua_tostring(L, -1));
+					}
+					lua_pop(L, 2);
+					/* Stack: key, table */
+				}
+				/* Stack: table */
+				lua_pop(L, 1);
+				/* Stack: <empty> */
+				break;
+			}
+			default: {
+				TRACE("(%d) [      ] unparsed type %s", i, lua_typename(L, t));
+				break;
+			}
 		}
-		printf(" "); /* put a separator */
 	}
-	printf("\n"); /* end the listing */
 }
 
 int run_lua_script(char *script, char *function, char *parms)
