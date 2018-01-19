sbabic/swupdate

commit 15a08c0081c5f2b012a5418bcf8403d557337141

Fix some compiler warnings if !CONFIG_HASH_VERIFY
Fix "statement with no effect [-Wunused-value]" and
"unused variable [-Wunused-variable]" warnings caused
by swupdate_HASH_update() and swupdate_HASH_final()
calls if !CONFIG_HASH_VERIFY.

Signed-off-by: Christian Storm <christian.storm@siemens.com>
Reviewed-by: Stefano Babic <sbabic@denx.de>

include/sslapi.h
@@ -91,8 +91,26 @@ int swupdate_HASH_compare(unsigned char *hash1, unsigned char *hash2);
 #define swupdate_dgst_init(sw, keyfile) ( 0 )
 #define swupdate_HASH_init(p) ( NULL )
 #define swupdate_verify_file(dgst, sigfile, file) ( 0 )
-#define swupdate_HASH_update(p, buf, len)	(-1)
-#define swupdate_HASH_final(p, result, len)	(-1)
+static inline int swupdate_HASH_update(struct swupdate_digest *dgst,
+				       unsigned char *buf, size_t len);
+static inline int swupdate_HASH_update(struct swupdate_digest *dgst,
+				       unsigned char *buf, size_t len)
+{
+	(void)dgst;
+	(void)buf;
+	(void)len;
+	return -1;
+}
+static inline int swupdate_HASH_final(struct swupdate_digest *dgst,
+				      unsigned char *md_value, unsigned int *md_len);
+static inline int swupdate_HASH_final(struct swupdate_digest *dgst,
+				      unsigned char *md_value, unsigned int *md_len)
+{
+	(void)dgst;
+	(void)md_value;
+	(void)md_len;
+	return -1;
+}
 #define swupdate_HASH_cleanup(sw)
 #define swupdate_HASH_compare(hash1,hash2)	(0)
 #endif

commit ac313f8e9851401784f64df39d0b522fc15adec5

Fix compilation error
The commit 15a08c0 has introduced a compilation error when the following
condition is met:

	!defined(CONFIG_HASH_VERIFY) && !defined(CONFIG_ENCRYPTED_IMAGES) && !defined(CONFIG_SURICATTA_SSL)

In that situation the structure swupdate_digest is never defined and the
compiler raises the following warnings and errors:

	  CC      core/swupdate.o
	In file included from core/swupdate.c:43:0:
	include/sslapi.h:94:47: warning: ‘struct swupdate_digest’ declared inside parameter list will not be visible outside of this definition or declaration
	 static inline int swupdate_HASH_update(struct swupdate_digest *dgst,
	                                               ^~~~~~~~~~~~~~~
	include/sslapi.h:96:47: warning: ‘struct swupdate_digest’ declared inside parameter list will not be visible outside of this definition or declaration
	 static inline int swupdate_HASH_update(struct swupdate_digest *dgst,
	                                               ^~~~~~~~~~~~~~~
	include/sslapi.h:96:19: error: conflicting types for ‘swupdate_HASH_update’
	 static inline int swupdate_HASH_update(struct swupdate_digest *dgst,
	                   ^~~~~~~~~~~~~~~~~~~~
	include/sslapi.h:94:19: note: previous declaration of ‘swupdate_HASH_update’ was here
	 static inline int swupdate_HASH_update(struct swupdate_digest *dgst,
	                   ^~~~~~~~~~~~~~~~~~~~
	include/sslapi.h:104:46: warning: ‘struct swupdate_digest’ declared inside parameter list will not be visible outside of this definition or declaration
	 static inline int swupdate_HASH_final(struct swupdate_digest *dgst,
	                                              ^~~~~~~~~~~~~~~
	include/sslapi.h:106:46: warning: ‘struct swupdate_digest’ declared inside parameter list will not be visible outside of this definition or declaration
	 static inline int swupdate_HASH_final(struct swupdate_digest *dgst,
	                                              ^~~~~~~~~~~~~~~

include/sslapi.h
@@ -70,6 +70,7 @@ struct swupdate_digest {
 #endif
 
 #else
+struct swupdate_digest;
 #define swupdate_crypto_init()
 #define AES_BLOCK_SIZE	16
 #endif