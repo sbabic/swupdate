sbabic/swupdate

commit 7f8b34f7f839c4d556621581206bef22ac7a0921

constify string_split input
Let the passed string unchanged - copy the input via strdup because
string is used in strtok() calls.

Signed-off-by: Stefano Babic <sbabic@denx.de>

include/util.h
@@ -10,6 +10,7 @@
 
 #include <stdint.h>
 #include <string.h>
+#include <linux/types.h>
 #include "swupdate.h"
 #include "swupdate_status.h"
 #include "compat.h"
@@ -148,10 +149,11 @@ void notify_init(void);
 int syslog_init(void);
 
 char **splitargs(char *args, int *argc);
-char** string_split(char* a_str, const char a_delim);
+char** string_split(const char* a_str, const char a_delim);
 void freeargs (char **argv);
 int get_hw_revision(struct hw_type *hw);
 void get_sw_versions(char *cfgfname, struct swupdate_cfg *sw);
+__u64 version_to_number(const char *version_string);
 int check_hw_compatibility(struct swupdate_cfg *cfg);
 int count_elem_list(struct imglist *list);
 

core/util.c
@@ -426,15 +426,18 @@ unsigned char *get_aes_salt(void) {
 	return aes_key->salt;
 }
 
-char** string_split(char* s, const char d)
+char** string_split(const char* in, const char d)
 {
 	char** result    = 0;
 	size_t count     = 0;
-	char* tmp        = s;
 	char* last_delim = 0;
 	char delim[2];
 	delim[0] = d;
 	delim[1] = 0;
+	char *s = strdup(in);
+	char* tmp        = s;
+	if (!s)
+		return NULL;
 
 	/* Count how many elements will be extracted. */
 	while (*tmp)
@@ -468,6 +471,8 @@ char** string_split(char* s, const char d)
 	    }
 	    *(result + idx) = 0;
 	}
+
+	free(s);
 
 	return result;
 }

commit 6f34b96842e9752a45746f441a29446e109d20d2

Fix compilation on FreeBSD broken by __u64 usage
Commits c85acb3 and 7f8b34f broke compilation on
FreeBSD because the BSDs don't define __u64.

Signed-off-by: Christian Storm <christian.storm@siemens.com>

corelib/artifacts_versions.c
@@ -13,7 +13,10 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/stat.h>
+#if defined(__linux__)
 #include <linux/types.h>
+#endif
+#include <compat.h>
 #include <limits.h>
 #include <assert.h>
 #include "generated/autoconf.h"

include/compat.h
@@ -31,4 +31,9 @@
  * 61 on FreeBSD is ECONNREFUSED.
  */
 #define ENODATA ENOATTR
+
+/*
+ * The BSDs don't define this while Linux does.
+ */
+#define __u64 uint64_t
 #endif

include/util.h
@@ -10,7 +10,9 @@
 
 #include <stdint.h>
 #include <string.h>
+#if defined(__linux__)
 #include <linux/types.h>
+#endif
 #include "swupdate.h"
 #include "swupdate_status.h"
 #include "compat.h"