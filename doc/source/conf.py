d71f649c741a35ecdad91af67533870764e421ce
Christian Storm <christian.storm@siemens.com>
2018-10-29 10:47:18 +0100

    doc: make sphinx > 1.4 use sphinx.ext.imgmath
    While sphinx.ext.pngmath was deprecated but still supported
    for a while, sphinx 1.8.0 removed support for it. In order
    to not break old sphinx installations, choose the extension
    sphinx.ext.{imgmath,pngmath} depending on sphinx's version.
    
    Signed-off-by: Christian Storm <christian.storm@siemens.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,7 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys, os, sphinx, distutils.version
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -25,8 +25,12 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.pngmath']
+extensions = ['sphinx.ext.todo']
 
+if distutils.version.LooseVersion(sphinx.__version__) < distutils.version.LooseVersion('1.4'):
+    extensions.append('sphinx.ext.pngmath')
+else:
+    extensions.append('sphinx.ext.imgmath')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = [u'_templates']
