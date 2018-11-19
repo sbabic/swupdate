sbabic/swupdate

commit b72c0a4784d095507f408d5580c0bd0b44104cc9

signature: allow to verify signers common name
Add a command line option and string config "forced-signer-name"
to specify the expected common name of signers certificate.

Signed-off-by: Denis Osterland <Denis.Osterland@diehl.com>
Tested-by: Stefano Babic <sbabic@denx.de>

corelib/verify_signature.c
@@ -109,7 +109,7 @@ static int verify_final(struct swupdate_digest *dgst, unsigned char *sig, unsign
 }
 
 int swupdate_verify_file(struct swupdate_digest *dgst, const char *sigfile,
-		const char *file)
+		const char *file, const char *signer_name)
 {
 	FILE *fp = NULL;
 	BIO *sigbio;
@@ -121,6 +121,7 @@ int swupdate_verify_file(struct swupdate_digest *dgst, const char *sigfile,
 	size_t rbytes;
 	int status = 0;
 
+	(void)signer_name;
 	if (!dgst) {
 		ERROR("Wrong crypto initialization: did you pass the key ?");
 		status = -ENOKEY;
@@ -292,8 +293,74 @@ static X509_STORE *load_cert_chain(const char *file)
 	return castore;
 }
 
+static inline int next_common_name(X509_NAME *subject, int i)
+{
+	return X509_NAME_get_index_by_NID(subject, NID_commonName, i);
+}
+
+static int check_common_name(X509_NAME *subject, const char *name)
+{
+	int i = -1, ret = 1;
+
+	while ((i = next_common_name(subject, i)) > -1) {
+		X509_NAME_ENTRY *e = X509_NAME_get_entry(subject, i);
+		ASN1_STRING *d = X509_NAME_ENTRY_get_data(e);
+		unsigned char *cn;
+		size_t len = strlen(name);
+		bool matches = (ASN1_STRING_to_UTF8(&cn, d) == (int)len)
+				&& (strncmp(name, (const char *)cn, len) == 0);
+
+		OPENSSL_free(cn);
+		if (!matches) {
+			char *subj = X509_NAME_oneline(subject, NULL, 0);
+
+			ERROR("common name of '%s' does not match expected '%s'",
+				subj, name);
+			OPENSSL_free(subj);
+			return 2;
+		} else {
+			ret = 0;
+		}
+	}
+
+	if (ret == 0) {
+		char *subj = X509_NAME_oneline(subject, NULL, 0);
+
+		TRACE("verified signer cert: %s", subj);
+		OPENSSL_free(subj);
+	}
+
+	return ret;
+}
+
+static int check_signer_name(CMS_ContentInfo *cms, const char *name)
+{
+	STACK_OF(CMS_SignerInfo) *infos = CMS_get0_SignerInfos(cms);
+	STACK_OF(X509) *crts = CMS_get1_certs(cms);
+	int i, ret = 1;
+
+	if ((name == NULL) || (name[0] == '\0'))
+		return 0;
+
+	for (i = 0; i < sk_CMS_SignerInfo_num(infos); ++i) {
+		CMS_SignerInfo *si = sk_CMS_SignerInfo_value(infos, i);
+		int j;
+
+		for (j = 0; j < sk_X509_num(crts); ++j) {
+			X509 *crt = sk_X509_value(crts, j);
+
+			if (CMS_SignerInfo_cert_cmp(si, crt) == 0) {
+				ret = check_common_name(
+					X509_get_subject_name(crt), name);
+			}
+		}
+	}
+
+	return ret;
+}
+
 int swupdate_verify_file(struct swupdate_digest *dgst, const char *sigfile,
-		const char *file)
+		const char *file, const char *signer_name)
 {
 	int status = -EFAULT;
 	CMS_ContentInfo *cms = NULL;
@@ -311,6 +378,12 @@ int swupdate_verify_file(struct swupdate_digest *dgst, const char *sigfile,
 	cms = d2i_CMS_bio(sigfile_bio, NULL);
 	if (!cms) {
 		ERROR("%s cannot be parsed as DER-encoded CMS signature blob", sigfile);
+		status = -EFAULT;
+		goto out;
+	}
+
+	if (check_signer_name(cms, signer_name)) {
+		ERROR("failed to verify signer name");
 		status = -EFAULT;
 		goto out;
 	}

core/parser.c
@@ -227,7 +227,8 @@ int parse(struct swupdate_cfg *sw, const char *descfile)
 	strcpy(sigfile, descfile);
 	strcat(sigfile, ".sig");
 
-	ret = swupdate_verify_file(sw->dgst, sigfile, descfile);
+	ret = swupdate_verify_file(sw->dgst, sigfile, descfile,
+				   sw->globals.forced_signer_name);
 	free(sigfile);
 
 	if (ret)

core/swupdate.c
@@ -82,6 +82,7 @@ static struct option long_options[] = {
 #ifdef CONFIG_SIGNED_IMAGES
 	{"key", required_argument, NULL, 'k'},
 	{"cert-purpose", required_argument, NULL, '1'},
+	{"forced-signer-name", required_argument, NULL, '2'},
 #endif
 #ifdef CONFIG_ENCRYPTED_IMAGES
 	{"key-aes", required_argument, NULL, 'K'},
@@ -129,6 +130,7 @@ static void usage(char *programname)
 		" -k, --key <public key file>    : file with public key to verify images\n"
 		"     --cert-purpose <purpose>   : set expected certificate purpose\n"
 		"                                  [emailProtection|codeSigning] (default: emailProtection)\n"
+		"     --forced-signer-name <cn>  : set expected common name of signer certificate\n"
 #endif
 #ifdef CONFIG_ENCRYPTED_IMAGES
 		" -K, --key-aes <key file>       : the file contains the symmetric key to be used\n"
@@ -505,6 +507,8 @@ static int read_globals_settings(void *elem, void *data)
 				"cert-purpose", tmp);
 	if (tmp[0] != '\0')
 		sw->globals.cert_purpose = parse_cert_purpose(tmp);
+	GET_FIELD_STRING(LIBCFG_PARSER, elem, "forced-signer-name",
+				sw->globals.forced_signer_name);
 
 	return 0;
 }
@@ -720,6 +724,10 @@ int main(int argc, char **argv)
 			break;
 		case '1':
 			swcfg.globals.cert_purpose = parse_cert_purpose(optarg);
+			break;
+		case '2':
+			strncpy(swcfg.globals.forced_signer_name, optarg,
+				sizeof(swcfg.globals.forced_signer_name));
 			break;
 #ifdef CONFIG_ENCRYPTED_IMAGES
 		case 'K':

include/sslapi.h
@@ -109,7 +109,7 @@ int swupdate_HASH_final(struct swupdate_digest *dgst, unsigned char *md_value,
 	       			unsigned int *md_len);
 void swupdate_HASH_cleanup(struct swupdate_digest *dgst);
 int swupdate_verify_file(struct swupdate_digest *dgst, const char *sigfile,
-	       	const char *file);
+				const char *file, const char *signer_name);
 int swupdate_HASH_compare(unsigned char *hash1, unsigned char *hash2);
 
 

include/swupdate.h
@@ -114,6 +114,7 @@ struct swupdate_global_cfg {
 	char postupdatecmd[SWUPDATE_GENERAL_STRING_SIZE];
 	char current_version[SWUPDATE_GENERAL_STRING_SIZE];
 	int cert_purpose;
+	char forced_signer_name[SWUPDATE_GENERAL_STRING_SIZE];
 };
 
 struct swupdate_cfg {

commit 1963f52145156d3b77d85cc32587511c46ee0a9f

signature: fix memory leak
CMS_get1_certs returns a newly allocated pointer,
which has to be freed by caller.

Fixes: b72c0a4784d095507f408d5580c0bd0b44104cc9
Signed-off-by: Denis Osterland <Denis.Osterland@diehl.com>
Acked-by: Stefano Babic <sbabic@denx.de>

corelib/verify_signature.c
@@ -336,12 +336,13 @@ static int check_common_name(X509_NAME *subject, const char *name)
 static int check_signer_name(CMS_ContentInfo *cms, const char *name)
 {
 	STACK_OF(CMS_SignerInfo) *infos = CMS_get0_SignerInfos(cms);
-	STACK_OF(X509) *crts = CMS_get1_certs(cms);
+	STACK_OF(X509) *crts;
 	int i, ret = 1;
 
 	if ((name == NULL) || (name[0] == '\0'))
 		return 0;
 
+	crts = CMS_get1_certs(cms);
 	for (i = 0; i < sk_CMS_SignerInfo_num(infos); ++i) {
 		CMS_SignerInfo *si = sk_CMS_SignerInfo_value(infos, i);
 		int j;
@@ -355,6 +356,7 @@ static int check_signer_name(CMS_ContentInfo *cms, const char *name)
 			}
 		}
 	}
+	sk_X509_pop_free(crts, X509_free);
 
 	return ret;
 }