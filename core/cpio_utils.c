sbabic/swupdate

commit 36c15ef3e701cc2466b0d742a00c0e6211539f1f

cpio_utils: fix infinite read when interrupting an update
When using the IPC API to do an update from a stream using a compressed
image, a new update cannot be started if the previous one is
interrupted.

The root cause is: When the client closes the socket, the read on the
server side will return 0 and it is treated as a successful read in
gunzip_step(). Thus, it goes into an infinite read loop.

It is fixed by adhering to the contract that an upstream step return
zero in case of EOF.

It can be reproduced using a compressed image:

1. Start the server via "swupdate -e stable,image2".
2. Trigger an update via "swupdate-client image.swu".
3. Cancel the update via Ctrl + C.
4. Trigger another update via "swupdate-client image.swu".

The following is the sw-description file used for the test:

software =
{
    version = "4.0.0";

    hardware-compatibility: ["4.0"];

    stable = {
        image1: {
            images: (
            {
                filename = "rootfs.ext4.gz";
                device = "/dev/mmcblk2p1";
                compressed = true;
                installed-directly = true;
                sha256 = "@SHA256SUM@";
            }
            );
        };
        image2: {
            images: (
            {
                filename = "rootfs.ext4.gz";
                device = "/dev/mmcblk2p2";
                compressed = true;
                installed-directly = true;
                sha256 = "@SHA256SUM@";
            }
            );
        };
    };
}

Signed-off-by: Ben Guan <ben.guan@cn.bosch.com>
Signed-off-by: Mark Jonas <mark.jonas@de.bosch.com>
Acked-by: Stefano Babic <sbabic@denx.de>

core/cpio_utils.c
@@ -135,7 +135,7 @@ static int input_step(void *state, void *buffer, size_t size)
 		size = s->nbytes;
 	}
 	int ret = fill_buffer(s->fdin, buffer, size, s->offs, &s->checksum, s->dgst);
-	if (ret < 0) {
+	if (ret <= 0) {
 		return ret;
 	}
 	s->nbytes -= size;
@@ -173,6 +173,8 @@ static int decrypt_step(void *state, void *buffer, size_t size)
 	ret = s->upstream_step(s->upstream_state, s->input, sizeof s->input);
 	if (ret < 0) {
 		return ret;
+	} else if (ret == 0) {
+		s->eof = true;
 	}
 
 	inlen = ret;
@@ -234,6 +236,8 @@ static int gunzip_step(void *state, void *buffer, size_t size)
 			ret = s->upstream_step(s->upstream_state, s->input, sizeof s->input);
 			if (ret < 0) {
 				return ret;
+			} else if (ret == 0) {
+				s->eof = true;
 			}
 			s->strm.avail_in = ret;
 			s->strm.next_in = s->input;
@@ -317,7 +321,7 @@ int copyfile(int fdin, void *out, unsigned int nbytes, unsigned long *offs, unsi
 		if (!input_state.dgst)
 			return -EFAULT;
 	}
- 
+
 	if (encrypted) {
 		aes_key = get_aes_key();
 		ivt = get_aes_ivt();

commit 8c8db83ba3ee0b017321d8977f382b9c0b14a7bf

cpio_utils: fix encryption
36c15ef3e701cc2466b0d742a00c0e6211539f1f introduced a bug when an
artifact is encrypted because the finalize() function is not called when
the input stream returns 0.

Signed-off-by: Ben Guan <ben.guan@cn.bosch.com>
Signed-off-by: Mark Jonas <mark.jonas@de.bosch.com>
Signed-off-by: Stefano Babic <sbabic@denx.de>
Tested-by: Stefano Babic <sbabic@denx.de>
CC: Richard OGrady <rogrady@sbcglobal.net>

core/cpio_utils.c
@@ -135,10 +135,10 @@ static int input_step(void *state, void *buffer, size_t size)
 		size = s->nbytes;
 	}
 	int ret = fill_buffer(s->fdin, buffer, size, s->offs, &s->checksum, s->dgst);
-	if (ret <= 0) {
+	if (ret < 0) {
 		return ret;
 	}
-	s->nbytes -= size;
+	s->nbytes -= ret;
 	return ret;
 }
 
@@ -173,8 +173,6 @@ static int decrypt_step(void *state, void *buffer, size_t size)
 	ret = s->upstream_step(s->upstream_state, s->input, sizeof s->input);
 	if (ret < 0) {
 		return ret;
-	} else if (ret == 0) {
-		s->eof = true;
 	}
 
 	inlen = ret;