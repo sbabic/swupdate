bdc11f9e6fd0497fa3adeb1feacc160d655aedff
Stefano Babic <sbabic@denx.de>
2018-12-08 14:09:47 +0100

    ucfwhandler: workaround for libgpiod changes
    libgpiod changed API, but there is no way to get the version at build
    time. gpiod_line_request_output() was replaced by by gpiod_line_request_flags_output()
    while gpiod_line_request_output() does not have the flags parameter
    anymore (not used by the handler).
    
    A compiler flag in SWUpdate is added to handle different versions of the
    library.
    
    Signed-off-by: Stefano Babic <sbabic@denx.de>

--- handlers/ucfw_handler.c
+++ handlers/ucfw_handler.c
@@ -139,13 +139,21 @@ static int switch_mode(char *devreset, int resoffset, char *devprog, int progoff
 		goto freegpios;
 	}
 
+#ifdef CONFIG_UCFW_OLD_LIBGPIOD
 	status = gpiod_line_request_output(linereset, RESET_CONSUMER, false, 0);
+#else
+	status = gpiod_line_request_output(linereset, RESET_CONSUMER, 0);
+#endif
 	if (status) {
 		ret  =-ENODEV;
 		ERROR("Cannot request reset line");
 		goto freegpios;
 	}
+#ifdef CONFIG_UCFW_OLD_LIBGPIOD
 	status = gpiod_line_request_output(lineprog, PROG_CONSUMER, false, mode);
+#else
+	status = gpiod_line_request_output(lineprog, PROG_CONSUMER, mode);
+#endif
 	if (status) {
 		ret  =-ENODEV;
 		ERROR("Cannot request prog line");
