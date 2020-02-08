sbabic/swupdate

commit 07811c7b570ae8a75d78f39bb75baf840cfad656

swupdate: add pre-update command hook
The --preupdate (-P) parameter allows to specify a command to run before
initiating the upgrade.

Signed-off-by: Antoine Girard-Vallée <antoine.girard-vallee@savoirfairelinux.com>

corelib/stream_interface.c
@@ -523,6 +523,10 @@ void *network_initializer(void *data)
 		}
 		close(inst.fd);
 
+		if (ret == 0) {
+			ret = preupdatecmd(software);
+		}
+
 		/* do carry out the installation (flash programming) */
 		if (ret == 0) {
 			TRACE("Valid image found: copying to FLASH");

core/swupdate.c
@@ -108,6 +108,7 @@ static struct option long_options[] = {
 #endif
 	{"check", no_argument, NULL, 'c'},
 	{"postupdate", required_argument, NULL, 'p'},
+	{"preupdate", required_argument, NULL, 'P'},
 	{NULL, 0, NULL, 0}
 };
 
@@ -124,6 +125,7 @@ static void usage(char *programname)
 		" -b, --blacklist <list of mtd>  : MTDs that must not be scanned for UBI\n"
 #endif
 		" -p, --postupdate               : execute post-update command\n"
+		" -P, --preupdate                : execute pre-update command\n"
 		" -e, --select <software>,<mode> : Select software images set and source\n"
 		"                                  Ex.: stable,main\n"
 		" -i, --image <filename>         : Software to be installed\n"
@@ -364,6 +366,12 @@ static int install_from_file(char *fname, int check)
 		exit(EXIT_SUCCESS);
 	}
 
+	ret = preupdatecmd(&swcfg);
+	if (ret) {
+		ERROR("Failed pre-update command!");
+		exit(EXIT_FAILURE);
+	}
+
 #ifdef CONFIG_MTD
 		mtd_cleanup();
 		scan_mtd_devices();
@@ -507,6 +515,8 @@ static int read_globals_settings(void *elem, void *data)
 				"mtd-blacklist", sw->globals.mtdblacklist);
 	GET_FIELD_STRING(LIBCFG_PARSER, elem,
 				"postupdatecmd", sw->globals.postupdatecmd);
+	GET_FIELD_STRING(LIBCFG_PARSER, elem,
+				"preupdatecmd", sw->globals.preupdatecmd);
 	get_field(LIBCFG_PARSER, elem, "verbose", &sw->globals.verbose);
 	get_field(LIBCFG_PARSER, elem, "loglevel", &sw->globals.loglevel);
 	get_field(LIBCFG_PARSER, elem, "syslog", &sw->globals.syslog_enabled);
@@ -610,7 +620,7 @@ int main(int argc, char **argv)
 #endif
 	memset(main_options, 0, sizeof(main_options));
 	memset(image_url, 0, sizeof(image_url));
-	strcpy(main_options, "vhni:e:l:Lcf:p:o:N:R:M");
+	strcpy(main_options, "vhni:e:l:Lcf:p:P:o:N:R:M");
 #ifdef CONFIG_MTD
 	strcat(main_options, "b:");
 #endif
@@ -822,6 +832,10 @@ int main(int argc, char **argv)
 		case 'p':
 			strncpy(swcfg.globals.postupdatecmd, optarg,
 				sizeof(swcfg.globals.postupdatecmd));
+			break;
+		case 'P':
+			strncpy(swcfg.globals.preupdatecmd, optarg,
+				sizeof(swcfg.globals.preupdatecmd));
 			break;
 		default:
 			usage(argv[0]);

corelib/installer.c
@@ -462,21 +462,39 @@ void cleanup_files(struct swupdate_cfg *software) {
 #endif
 }
 
+static int run_system_cmd(const char *cmd, const char *desc)
+{
+	int ret = 0;
+	if ((strnlen(cmd, SWUPDATE_GENERAL_STRING_SIZE) > 0)
+		&& (strnlen(cmd, SWUPDATE_GENERAL_STRING_SIZE) < SWUPDATE_GENERAL_STRING_SIZE)) {
+		DEBUG("Running %s command '%s' ...", desc, cmd);
+		ret = system(cmd);
+		if (WIFEXITED(ret)) {
+			DEBUG("%s command returned %d", desc, WEXITSTATUS(ret));
+		} else {
+			ERROR("%s command returned %d: '%s'", desc, ret, strerror(errno));
+			return -1;
+		}
+	}
+
+	return ret;
+}
+
+int preupdatecmd(struct swupdate_cfg *swcfg)
+{
+	if (swcfg) {
+		return run_system_cmd(swcfg->globals.preupdatecmd, "Pre-update");
+	}
+
+	return 0;
+}
+
 int postupdate(struct swupdate_cfg *swcfg, const char *info)
 {
 	swupdate_progress_done(info);
 
-	if ((swcfg) && (strnlen(swcfg->globals.postupdatecmd,
-		     SWUPDATE_GENERAL_STRING_SIZE) > 0)) {
-		DEBUG("Executing post-update command '%s'",
-		      swcfg->globals.postupdatecmd);
-		int ret = system(swcfg->globals.postupdatecmd);
-		if (WIFEXITED(ret)) {
-			DEBUG("Post-update command returned %d", WEXITSTATUS(ret));
-		} else {
-			ERROR("Post-update command returned %d: '%s'", ret, strerror(errno));
-			return -1;
-		}
+	if ((swcfg) && (run_system_cmd(swcfg->globals.postupdatecmd, "Post-update") == -1)) {
+		return -1;
 	}
 
 	return 0;

examples/configuration/swupdate.cfg
@@ -23,6 +23,9 @@
 # aes-key-file		: string
 #			  file containing the symmetric key for
 #			  image decryption
+# preupdatecmd		: string
+#			  command to be executed right before the update
+#			  is installed
 # postupdatecmd		: string
 #			  command to be executed after a successful update
 globals :

include/installer.h
@@ -19,6 +19,7 @@ int check_if_required(struct imglist *list, struct filehdr *pfdh,
 int install_images(struct swupdate_cfg *sw, int fdsw, int fromfile);
 int install_single_image(struct img_type *img, int dry_run);
 int postupdate(struct swupdate_cfg *swcfg, const char *info);
+int preupdatecmd(struct swupdate_cfg *swcfg);
 void cleanup_files(struct swupdate_cfg *software);
 
 #endif

include/swupdate.h
@@ -118,6 +118,7 @@ struct swupdate_global_cfg {
 	char publickeyfname[SWUPDATE_GENERAL_STRING_SIZE];
 	char aeskeyfname[SWUPDATE_GENERAL_STRING_SIZE];
 	char postupdatecmd[SWUPDATE_GENERAL_STRING_SIZE];
+	char preupdatecmd[SWUPDATE_GENERAL_STRING_SIZE];
 	char minimum_version[SWUPDATE_GENERAL_STRING_SIZE];
 	char current_version[SWUPDATE_GENERAL_STRING_SIZE];
 	int cert_purpose;

commit 792a4af7c8adcd6a3ed961a213042f4c927c283e

swupdate: run pre-update command hook earlier
The pre-update command hook must be run from extract_files(). Otherwise
it will be too late for images in streaming mode.

Fixes: 07811c7b57 ("swupdate: add pre-update command hook")
Signed-off-by: Christian Eggers <ceggers@arri.de>

corelib/stream_interface.c
@@ -162,6 +162,9 @@ static int extract_files(int fd, struct swupdate_cfg *software)
 				ERROR("SW not compatible with hardware");
 				return -1;
 			}
+			if (preupdatecmd(software)) {
+				return -1;
+			}
 			status = STREAM_DATA;
 			break;
 
@@ -521,10 +524,6 @@ void *network_initializer(void *data)
 			ret = extract_files(inst.fd, software);
 		}
 		close(inst.fd);
-
-		if (ret == 0) {
-			ret = preupdatecmd(software);
-		}
 
 		/* do carry out the installation (flash programming) */
 		if (ret == 0) {