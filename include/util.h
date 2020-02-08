sbabic/swupdate

commit 0e49d983dd07d47befb672b674e95670379c9076

Factorize SETSTRING and STRINGIFY macros
These are used by more as one module, put them in util.h

Signed-off-by: Stefano Babic <sbabic@denx.de>

include/util.h
@@ -116,6 +116,13 @@ typedef void (*notifier) (RECOVERY_STATUS status, int error, int level, const ch
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
+#define STRINGIFY(...) #__VA_ARGS__
+#define SETSTRING(p, v) do { \
+	if (p) \
+		free(p); \
+	p = strdup(v); \
+} while (0)
+
 
 #define LG_16 4
 #define FROM_HEX(f) from_ascii (f, sizeof f, LG_16)

corelib/downloader.c
@@ -17,12 +17,6 @@
 #include "parselib.h"
 #include "swupdate_settings.h"
 
-#define SETSTRING(p, v) do { \
-	if (p) \
-		free(p); \
-	p = strdup(v); \
-} while (0)
-
 /*
  * Number of seconds while below low speed
  * limit before aborting. It can be overwritten

suricatta/server_hawkbit.c
@@ -31,14 +31,7 @@
 
 #define INITIAL_STATUS_REPORT_WAIT_DELAY 10
 
-#define STRINGIFY(...) #__VA_ARGS__
 #define JSON_OBJECT_FREED 1
-
-#define SETSTRING(p, v) do { \
-	if (p) \
-		free(p); \
-	p = strdup(v); \
-} while (0)
 
 static struct option long_options[] = {
     {"tenant", required_argument, NULL, 't'},

commit abdd6512d7a52fba3a02a3aa4f4f4df4ee063476

util: Add missing includes
- free() requires stdlib.h

Fixes: 0e49d983dd ("Factorize SETSTRING and STRINGIFY macros")
Signed-off-by: Christian Eggers <ceggers@arri.de>

include/util.h
@@ -9,6 +9,7 @@
 #define _UTIL_H
 
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #if defined(__linux__)
 #include <linux/types.h>