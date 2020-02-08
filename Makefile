sbabic/swupdate

commit 58a4c02953eb4e2bb4c290a60644dbbfeade33fd

Build documentation from main Makefile
Allow to build documentation just as target from
Makefile instead of going into doc/ directory.
Run make help for a list of all formats
for the generated documentation.

Signed-off-by: Stefano Babic <sbabic@denx.de>
Reviewed-by: Bjørn Forsman <bjorn.forsman@gmail.com>

Makefile
@@ -419,6 +419,7 @@ clean: $(clean-dirs)
 		-o -name '.*.d' -o -name '.*.tmp' -o -name '*.mod.c' \
 		-o -name modules.builtin -o -name '.tmp_*.o.*' \
 		-o -name '*.gcno' \) -type f -print | xargs rm -f
+	@make -C doc clean
 
 # mrproper - Delete all generated files, including .config
 #
@@ -469,6 +470,13 @@ endif	# skip-makefile
 
 PHONY += FORCE
 FORCE:
+
+# Documentation
+# run Makefile in doc directory
+
+dirhtml singlehtml pickle json htmlhelp qthelp devhelp epub \
+latex latexpdf text man changes linkcheck html doctest:
+	@make -C doc $@
 
 # Declare the contents of the .PHONY variable as phony.  We keep that
 # information in a variable so we can use it in if_changed and friends.

Makefile.help
@@ -25,3 +25,6 @@ help:
 	@echo 'Development:'
 	@echo '  randconfig		- generate a random configuration'
 	@echo
+	@echo 'Documentation:'
+	@make -C doc help
+

commit 88e6c3e451c29bebefa1dccc45dd1fbf0f39520f

Makefile: Fix out of tree build for documentation
The doc/Makefile expects to be run from the source directory (e.g. with
a given BUILDDIR parameter).

Tested with make targets "help", "clean" and "html" for in-tree and
out-of-tree build.

Fixes: 58a4c02953 ("Build documentation from main Makefile")
Signed-off-by: Christian Eggers <ceggers@arri.de>
Acked-by: Stefano Babic <sbabic@denx.de>

Makefile
@@ -534,7 +534,8 @@ clean: $(clean-dirs)
 		-o -name '.*.d' -o -name '.*.tmp' -o -name '*.mod.c' \
 		-o -name modules.builtin -o -name '.tmp_*.o.*' \
 		-o -name '*.gcno' \) -type f -print | xargs rm -f
-	@$(MAKE) -C doc clean
+	@pwd
+	$(Q)$(MAKE) -f $(srctree)/doc/Makefile BUILDDIR=$(CURDIR)/doc/build clean
 
 # mrproper - Delete all generated files, including .config
 #
@@ -581,17 +582,17 @@ clean := -f $(if $(KBUILD_SRC),$(srctree)/)scripts/Makefile.clean obj
 endif #ifeq ($(config-targets),1)
 endif #ifeq ($(mixed-targets),1)
 
-endif	# skip-makefile
-
-PHONY += FORCE
-FORCE:
-
 # Documentation
 # run Makefile in doc directory
 
 dirhtml singlehtml pickle json htmlhelp qthelp devhelp epub \
 latex latexpdf text man changes linkcheck html doctest:
-	@$(MAKE) -C doc $@
+	$(Q)$(MAKE) -C $(srctree)/doc BUILDDIR=$(CURDIR)/doc/build $@
+
+endif	# skip-makefile
+
+PHONY += FORCE
+FORCE:
 
 # Declare the contents of the .PHONY variable as phony.  We keep that
 # information in a variable so we can use it in if_changed and friends.

Makefile.help
@@ -26,5 +26,5 @@ help:
 	@echo '  randconfig		- generate a random configuration'
 	@echo
 	@echo 'Documentation:'
-	@make -C doc help
+	@$(MAKE) -f $(srctree)/doc/Makefile help
 