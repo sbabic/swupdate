b369d54855bf2430f4a4d007ca121545156e9502
Stefano Babic <sbabic@denx.de>
2020-10-26 14:39:20 +0100

    progress API: set define for info field size
    Signed-off-by: Stefano Babic <sbabic@denx.de>

--- include/progress_ipc.h
+++ include/progress_ipc.h
@@ -15,6 +15,7 @@
 extern "C" {
 #endif
 
+#define PRINFOSIZE	2048
 
 extern char* SOCKET_PROGRESS_PATH;
 
@@ -33,7 +34,7 @@ struct progress_msg {
 	char		hnd_name[64];	/* Name of running hanler */
 	sourcetype	source;		/* Interface that triggered the update */
 	unsigned int 	infolen;    	/* Len of data valid in info */
-	char		info[2048];   	/* additional information about install */
+	char		info[PRINFOSIZE]; /* additional information about install */
 };
 
 char *get_prog_socket(void);
