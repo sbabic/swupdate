a7f5568f9a648b195497c053f9112c722b0f475b
Stefano Babic <sbabic@denx.de>
2020-12-30 12:55:33 +0100

    Added missing copyright for strlcpy function
    strlcpy() does not exist in Linux and the function in SWUpdate was taken
    from FreeBSD code. Add the missing Copyright of the author.
    
    Signed-off-by: Stefano Babic <sbabic@denx.de>
    Reported-by: Bastian Germann <bage@linutronix.de>

--- core/util.c
+++ core/util.c
@@ -223,39 +223,6 @@ char *substring(const char *src, int first, int len) {
 	return s;
 }
 
-#if defined(__linux__)
-
-/*
- * Copy string src to buffer dst of size dsize.  At most dsize-1
- * chars will be copied.  Always NUL terminates (unless dsize == 0).
- * Returns strlen(src); if retval >= dsize, truncation occurred.
- */
-size_t
-strlcpy(char * __restrict dst, const char * __restrict src, size_t dsize)
-{
-	const char *osrc = src;
-	size_t nleft = dsize;
-
-	/* Copy as many bytes as will fit. */
-	if (nleft != 0) {
-		while (--nleft != 0) {
-			if ((*dst++ = *src++) == '\0')
-				break;
-		}
-	}
-
-	/* Not enough room in dst, add NUL and traverse rest of src. */
-	if (nleft == 0) {
-		if (dsize != 0)
-			*dst = '\0';		/* NUL-terminate dst */
-		while (*src++)
-			;
-	}
-
-	return(src - osrc - 1);	/* count does not include NUL */
-}
-#endif
-
 int openfileoutput(const char *filename)
 {
 	int fdout;
