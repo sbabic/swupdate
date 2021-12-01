sbabic/swupdate

commit 2c32873110503fffb188f9363457e34210d2c160

mongoose: Ignore PROGRESS messages
PROGRESS messages should not be passed to websocket clients since they
show up as "UNKNOWN". If a client want progress messages, a dedicated
JSON message should be used to transmit the information.

Signed-off-by: Joshua Watt <JPEWhacker@gmail.com>
Acked-by: Stefano Babic <sbabic@denx.de>

mongoose/mongoose_interface.c
@@ -200,6 +200,9 @@ static void *broadcast_progress_thread(void *data)
 		if (ret != sizeof(msg))
 			return NULL;
 
+		if (msg.status == PROGRESS)
+			continue;
+
 		if (msg.status != status || msg.status == FAILURE) {
 			status = msg.status;
 

commit c16303023b43a0de2b0c5b198b40190251c01d76

mongoose: Only ignore PROGRESS status
2c32873 ("mongoose: Ignore PROGRESS messages") broke reporting of update
progress because the PROGRESS messages can hold valid data besides the
status. Update the code to only suppress sending the UNKNOWN status
message, but still send the other messages.

Signed-off-by: Joshua Watt <JPEWhacker@gmail.com>
Tested-by: Stefano Babic <sbabic@denx.de>

mongoose/mongoose_interface.c
@@ -229,10 +229,8 @@ static void *broadcast_progress_thread(void *data)
 		if (ret != sizeof(msg))
 			return NULL;
 
-		if (msg.status == PROGRESS)
-			continue;
-
-		if (msg.status != status || msg.status == FAILURE) {
+		if (msg.status != PROGRESS &&
+		    (msg.status != status || msg.status == FAILURE)) {
 			status = msg.status;
 
 			snescape(escaped, sizeof(escaped), get_status_string(msg.status));