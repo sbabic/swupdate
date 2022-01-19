9c86492b7dffe14b4b04f9edc79615f790a6debd
Christian Storm <christian.storm@siemens.com>
2022-01-19 13:18:13 +0100

    server_general: fix line breaking and indentation in help text
    Signed-off-by: Christian Storm <christian.storm@siemens.com>

--- suricatta/server_general.c
+++ suricatta/server_general.c
@@ -536,9 +536,9 @@ void server_print_help(void)
 	    "\t  -y, --proxy         Use proxy. Either give proxy URL, else "
 	    "{http,all}_proxy env is tried.\n"
 	    "\t  -a, --custom-http-header <name> <value> Set custom HTTP header, "
-	    "appended to every HTTP request being sent."
-	    "\t  -n, --max-download-speed <limit>	Set download speed limit."
-		"Example: -n 100k; -n 1M; -n 100; -n 1G",
+	    "appended to every HTTP request being sent.\n"
+	    "\t  -n, --max-download-speed <limit>        Set download speed limit. "
+		"Example: -n 100k; -n 1M; -n 100; -n 1G\n",
 	    CHANNEL_DEFAULT_POLLING_INTERVAL, CHANNEL_DEFAULT_RESUME_TRIES,
 	    CHANNEL_DEFAULT_RESUME_DELAY);
 }
