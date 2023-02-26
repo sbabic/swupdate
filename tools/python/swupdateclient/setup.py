f81ac44bcdd714be4fdd24e8883213ca9d8ade46
Stefano Babic <sbabic@denx.de>
2023-02-26 15:46:56 +0100

    tools: missing setup.py for swupdateclient
    setup.py was not yet merged, fixed.
    
    Signed-off-by: Stefano Babic <sbabic@denx.de>

---
+++ tools/python/swupdateclient/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='swupdateclient',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/sbabic/swupdate/tools/python/swupdateclient',
+    license='GPLv3',
+    author='Stefano Babic',
+    author_email='stefano.babic@swupdate.org',
+    description='Python Client to update SWUpdate based devices',
+    entry_points={
+        "console_scripts": [
+            "swupdateclient=swupdateclient.main:main",
+        ],
+    },
+    python_requires=">=3.9",
+)
