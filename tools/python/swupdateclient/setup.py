05b186892041501e9c84adad46df4cc2da6cebce
Jan Feemers <jan.feemers@gmx.de>
2023-10-23 20:55:15 +0200

    refactor: apply black formatting to python files
    ran the cmd: `black swupdateclient`
    
    Signed-off-by: Jan Feemers <jan.feemers@gmx.de>

--- tools/python/swupdateclient/setup.py
+++ tools/python/swupdateclient/setup.py
@@ -4,14 +4,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='swupdateclient',
-    version='0.1',
+    name="swupdateclient",
+    version="0.1",
     packages=find_packages(),
-    url='https://github.com/sbabic/swupdate/tree/master/tools/python/swupdateclient',
-    license='GPL-2.0-only',
-    author='Stefano Babic',
-    author_email='stefano.babic@swupdate.org',
-    description='Python Client to update SWUpdate based devices',
+    url="https://github.com/sbabic/swupdate/tree/master/tools/python/swupdateclient",
+    license="GPL-2.0-only",
+    author="Stefano Babic",
+    author_email="stefano.babic@swupdate.org",
+    description="Python Client to update SWUpdate based devices",
     entry_points={
         "console_scripts": [
             "swupdateclient=swupdateclient.main:main",
