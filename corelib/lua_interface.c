sbabic/swupdate

commit dab98ec2109c4baf12602b738d0c52ac5c1a0076

Lua: add function emmcbootpart
Function returns the HW boot device of an eMMC.

Signed-off-by: Stefano Babic <stefano.babic@swupdate.org>

corelib/lua_interface.c
@@ -973,6 +973,22 @@ l_get_hw_exit:
 	return 1;
 }
 
+static int l_get_emmc_bootpart(lua_State *L)
+{
+	const char *device = luaL_checkstring(L, 1);
+	int active = -1, fd;
+
+	if (device) {
+		fd = open(device, O_RDONLY);
+		if (fd) {
+			active = emmc_get_active_bootpart(fd);
+			close(fd);
+		}
+	}
+	lua_pushinteger(L, active);
+	return 1;
+}
+
 static void lua_push_enum(lua_State *L, const char *name, int value)
 {
 	lua_pushstring(L, name);
@@ -1194,6 +1210,7 @@ static const luaL_Reg l_swupdate[] = {
         { "get_hw", l_get_hw },
         { "getversion", lua_get_swupdate_version },
         { "progress", lua_notify_progress },
+        { "emmcbootpart", l_get_emmc_bootpart },
         { NULL, NULL }
 };
 

commit c33e38dfa64ca7a70439b0f3cadf6ec8c8b71a08

Fix coverity #487521
This defect was introduced by dab98ec2.

Signed-off-by: Stefano Babic <stefano.babic@swupdate.org>

corelib/lua_interface.c
@@ -978,13 +978,19 @@ static int l_get_emmc_bootpart(lua_State *L)
 	const char *device = luaL_checkstring(L, 1);
 	int active = -1, fd;
 
-	if (device) {
-		fd = open(device, O_RDONLY);
-		if (fd) {
-			active = emmc_get_active_bootpart(fd);
-			close(fd);
-		}
+	if (!device) {
+		lua_pushnil(L);
+		return 1;
 	}
+
+	fd = open(device, O_RDONLY);
+	if (fd < 0) {
+		lua_pushnil(L);
+		return 1;
+	}
+
+	active = emmc_get_active_bootpart(fd);
+	close(fd);
 	lua_pushinteger(L, active);
 	return 1;
 }