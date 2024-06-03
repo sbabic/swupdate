8e806c0f92ff44ac8b8c1bddd8cb2b28a8518cdc
Storm, Christian <christian.storm@siemens.com>
2024-06-03 18:26:29 +0000

    FreeBSD: Properly typedef __{u,s}{8,16,32,64}
    While Linux does define these, the BSDs don't. So do
    properly typedef __{u,s}{8,16,32,64} to BSD-known types.
    
    Signed-off-by: Christian Storm <christian.storm@siemens.com>

--- include/compat.h
+++ include/compat.h
@@ -45,5 +45,13 @@
 /*
  * The BSDs don't define this while Linux does.
  */
-#define __u64 uint64_t
+#include <sys/types.h>
+typedef int8_t   __s8;
+typedef uint8_t  __u8;
+typedef int16_t  __s16;
+typedef uint16_t __u16;
+typedef int32_t  __s32;
+typedef uint32_t __u32;
+typedef int64_t  __s64;
+typedef uint64_t __u64;
 #endif
