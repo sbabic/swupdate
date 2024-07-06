sbabic/swupdate

commit da48265ad29f24d1eedf29145ea4c2382d0a0e00

suricatta process notification: improve ipc_get_status scheduling
The process notification thread gets messages from swupdate to submit
them to the hawkbit server.

Before this patch, ipc_get_status_timeout() was called in a tight loop,
probably with the assumption that it would wait until the timeout
happens if no message was available.
Unfortunately, the server just replies immediately that there is no
message, so this would just hammer the server in a tight loop.

Furthermore, on slow devices if the server takes more than 100ms to
reply we close our socket so the server would crash to sigpipe without
the previous fix.

Adjust this loop to:
- since ipc_get_status() always returns immediately the timeout is
useless, use the version without timeout.
- wait 100ms before calling ipc_get_status() again if no message was
available.

Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>
Acked-by: Stefano Babic <stefano.babic@swupdate.org>

suricatta/server_hawkbit.c
@@ -997,7 +997,7 @@ static void *process_notification_thread(void *data)
 	for (;;) {
 		ipc_message msg;
 		bool data_avail = false;
-		int ret = ipc_get_status_timeout(&msg, 100);
+		int ret = ipc_get_status(&msg);
 
 		data_avail = ret > 0 && (strlen(msg.data.status.desc) != 0);
 
@@ -1060,6 +1060,10 @@ static void *process_notification_thread(void *data)
 
 		if (stop && !data_avail)
 			break;
+
+		// wait a bit for next message...
+		if (!data_avail)
+			usleep(100000);
 	}
 
 	pthread_mutex_unlock(&notifylock);

commit 9ef80aefd3547c35d3f6bd706e59d3a97b0ef703

hawkbit: fix process notification not sending logs
ipc_get_status_timeout() returns the size of the message on success, but
ipc_get_status() returns 0 in this case (both return a negative value on
error)

Changing the function to use ipc_get_status() didn't update the return
value check: fix this, and also properly check for errors.
Also remove obsolete comment describing the return value.

Reported-by: James Hilliard <james.hilliard1@gmail.com>
Fixes: da48265ad29f ("suricatta process notification: improve ipc_get_status scheduling")
Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

suricatta/server_hawkbit.c
@@ -1003,7 +1003,12 @@ static void *process_notification_thread(void *data)
 		bool data_avail = false;
 		int ret = ipc_get_status(&msg);
 
-		data_avail = ret > 0 && (strlen(msg.data.status.desc) != 0);
+		if (ret < 0) {
+			ERROR("Error getting status, stopping notification thread");
+			stop = true;;
+		} else {
+			data_avail = (strlen(msg.data.status.desc) != 0);
+		}
 
 		/*
 		 * Mutex used to synchronize end of the thread
@@ -1017,12 +1022,6 @@ static void *process_notification_thread(void *data)
 
 		if (data_avail && msg.data.status.current == PROGRESS)
 			continue;
-		/*
-		 * If there is a message
-		 * ret > 0: data available
-		 * ret == 0: TIMEOUT, no more messages
-		 * ret < 0 : ERROR, exit
-		 */
 		if (data_avail && numdetails < MAX_DETAILS) {
 			for (int c = 0; c < strlen(msg.data.status.desc); c++) {
 				switch (msg.data.status.desc[c]) {