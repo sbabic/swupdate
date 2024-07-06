sbabic/swupdate

commit f3b1e1e26dfb787940729ef3ffe46b1fca1a8eca

hawkbit: check for validity of action_id
The "swupdate-ipc sendtohawkbit" tool allows to send a feedback to the
Hawkbit server with external data. Commit d770ad59e introduces that
SWUpdate can persistently store the action-id between runs, and this
worked after a reboot when SWUpdate is started again.

This patch introduces the same feature when IPC is used. The action-id
in IPC can be set to 0 or -1 (invalid for Hawkbit), and SWUpdate will
then check if a valid action_id is in the environment.

Signed-off-by: Stefano Babic <stefano.babic@swupdate.org>

suricatta/server_hawkbit.c
@@ -867,6 +867,7 @@ static void get_action_id_from_env(int *action_id)
 	 * Get the acction_id that corresponds to the done update if it was
 	 * stored.
 	 */
+	*action_id = -1;
 	char *action_str = swupdate_vars_get("action_id", NULL);
 	if (action_str) {
 		int tmp = ustrtoull(action_str, NULL, 10);
@@ -2023,11 +2024,12 @@ static server_op_res_t server_activation_ipc(ipc_message *msg)
 	int action_id = -1;
 	if (json_data) {
 		action_id = json_object_get_int(json_data);
-	} else {
+	}
+	if (action_id <= 0) { /* 0 is not a valid action id */
 		get_action_id_from_env(&action_id);
 	}
 
-	if (action_id < 0) {
+	if (action_id <= 0) {
 		ERROR("No action_id passed into JSON message and no action:_id in env");
 		return SERVER_EERR;
 	}

commit 725b6675a9c2e5e2cae2ced158e319222e968dfc

hawkbit: do not reset action_id when getting from env
server_handle_initial_state() firsts get an action id from the server
and then overrides it with the env value, but there is no check if there
had been no env value, sending a request to id -1 to server.

The commit that added this reset also added code to only call
get_action_id_from_env() if action_id <= 0, so it did not need to reset
it: just leave the status quo of using the last value if it wasn't set.

Fixes: f3b1e1e26dfb ("hawkbit: check for validity of action_id")
Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>
Acked-by: Stefano Babic <stefano.babic@swupdate.org>

suricatta/server_hawkbit.c
@@ -867,7 +867,6 @@ static void get_action_id_from_env(int *action_id)
 	 * Get the acction_id that corresponds to the done update if it was
 	 * stored.
 	 */
-	*action_id = -1;
 	char *action_str = swupdate_vars_get("action_id", NULL);
 	if (action_str) {
 		int tmp = ustrtoull(action_str, NULL, 10);