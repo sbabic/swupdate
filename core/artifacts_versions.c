sbabic/swupdate

commit 9ef80aefd3547c35d3f6bd706e59d3a97b0ef703

hawkbit: fix process notification not sending logs
ipc_get_status_timeout() returns the size of the message on success, but
ipc_get_status() returns 0 in this case (both return a negative value on
error)

Changing the function to use ipc_get_status() didn't update the return
value check: fix this, and also properly check for errors.
Also remove obsolete comment describing the return value.

Reported-by: James Hilliard <james.hilliard1@gmail.com>
Fixes: da48265ad29f ("suricatta process notification: improve ipc_get_status scheduling")
Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

suricatta/server_hawkbit.c
@@ -1003,7 +1003,12 @@ static void *process_notification_thread(void *data)
 		bool data_avail = false;
 		int ret = ipc_get_status(&msg);
 
-		data_avail = ret > 0 && (strlen(msg.data.status.desc) != 0);
+		if (ret < 0) {
+			ERROR("Error getting status, stopping notification thread");
+			stop = true;;
+		} else {
+			data_avail = (strlen(msg.data.status.desc) != 0);
+		}
 
 		/*
 		 * Mutex used to synchronize end of the thread
@@ -1017,12 +1022,6 @@ static void *process_notification_thread(void *data)
 
 		if (data_avail && msg.data.status.current == PROGRESS)
 			continue;
-		/*
-		 * If there is a message
-		 * ret > 0: data available
-		 * ret == 0: TIMEOUT, no more messages
-		 * ret < 0 : ERROR, exit
-		 */
 		if (data_avail && numdetails < MAX_DETAILS) {
 			for (int c = 0; c < strlen(msg.data.status.desc); c++) {
 				switch (msg.data.status.desc[c]) {

commit 6b9a6bb96bacc565b0f9b0e7b610ccd0f48b7041

misc: remove double-semicolons
The previous commit 9ef80aefd354 ("hawkbit: fix process notification not
sending logs") introduced a typo using two semicolons after a statement.

While looking I also found another one.

This is not harmful, but is bad style: remove them.

Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

core/artifacts_versions.c
@@ -200,7 +200,7 @@ static bool is_oldstyle_version(const char *version_string, __u64 *version_numbe
 			return false;
 		++ver;
 	}
-	return version_to_number(version_string, version_number);;
+	return version_to_number(version_string, version_number);
 }
 
 /*

suricatta/server_hawkbit.c
@@ -1004,7 +1004,7 @@ static void *process_notification_thread(void *data)
 
 		if (ret < 0) {
 			ERROR("Error getting status, stopping notification thread");
-			stop = true;;
+			stop = true;
 		} else {
 			data_avail = (strlen(msg.data.status.desc) != 0);
 		}