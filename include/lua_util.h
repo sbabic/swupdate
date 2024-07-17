sbabic/swupdate

commit f91abdc8674dadf8d82f0ba035aecca9059d35cb

Lua: Sessionize Lua Handlers
Initialize the global Lua state and check whether built-in or
external file Lua handlers do load successfully, session scoped.

With this change, Lua handlers are loaded into Lua session
states and no longer (on startup) into the global Lua state.

The global Lua state is still around to be able to register
global Lua Handlers from the C realm, programmatically.

Signed-off-by: Christian Storm <christian.storm@siemens.com>

include/lua_util.h
@@ -22,6 +22,7 @@ typedef enum {
 void LUAstackDump (lua_State *L);
 int run_lua_script(lua_State *L, const char *script, bool load, const char *function, char *parms);
 lua_State *lua_session_init(struct dict *bootenv);
+int lua_init(void);
 int lua_load_buffer(lua_State *L, const char *buf);
 int lua_parser_fn(lua_State *L, const char *fcn, struct img_type *img);
 int lua_handler_fn(lua_State *L, const char *fcn, const char *parms);
@@ -101,6 +102,7 @@ struct img_type;
 #define lua_exit(L)
 #define lua_close(L)
 static inline lua_State *lua_session_init(struct dict __attribute__ ((__unused__)) *bootenv) { return NULL;}
+static inline int lua_init() { return 0; }
 static inline int lua_load_buffer(lua_State __attribute__ ((__unused__)) *L, 
 					const char __attribute__ ((__unused__)) *buf) {return 1;}
 static inline int lua_parser_fn(lua_State __attribute__ ((__unused__)) *L,

core/swupdate.c
@@ -861,7 +861,7 @@ int main(int argc, char **argv)
 		}
 	}
 
-	lua_handlers_init(NULL);
+	lua_init();
 
 	if(!get_hw_revision(&swcfg.hw))
 		INFO("Running on %s Revision %s", swcfg.hw.boardname, swcfg.hw.revision);

corelib/lua_interface.c
@@ -1541,12 +1541,8 @@ int lua_handlers_init(lua_State *L)
 #endif
 	int ret = -1;
 
-	if (!L) {
-		gL = luaL_newstate();
-		L = gL;
-	}
 	if (L) {
-		/* prime gL as LUA_TYPE_HANDLER */
+		/* prime L as LUA_TYPE_HANDLER */
 		lua_pushlightuserdata(L, (void*)LUA_TYPE_HANDLER);
 		lua_setglobal(L, "SWUPDATE_LUA_TYPE");
 		/* load standard libraries */
@@ -1601,6 +1597,18 @@ lua_State *lua_session_init(struct dict *bootenv)
 	lua_handlers_init(L);
 
 	return L;
+}
+
+int lua_init(void)
+{
+	lua_State *L = luaL_newstate();
+	int res = lua_handlers_init(L);
+	unregister_session_handlers();
+	lua_close(L);
+	if (!gL) {
+		gL = luaL_newstate();
+	}
+	return res;
 }
 
 int lua_load_buffer(lua_State *L, const char *buf)

commit a204ea312460acb1e99a00bc73edfd336b14749f

Lua: fix compiler warning
In contrast to C++, the C language requires an explicit 'void' if a
function expects no arguments.

Fixes: f91abdc8674d ("Lua: Sessionize Lua Handlers")
Signed-off-by: Christian Eggers <ceggers@arri.de>
Reviewed-by: Michael Glembotzki <Michael.Glembotzki@iris-sensing.com>

include/lua_util.h
@@ -101,7 +101,7 @@ struct img_type;
 #define lua_exit(L)
 #define lua_close(L)
 static inline lua_State *lua_session_init(struct dict __attribute__ ((__unused__)) *bootenv) { return NULL;}
-static inline int lua_init() { return 0; }
+static inline int lua_init(void) { return 0; }
 static inline int lua_load_buffer(lua_State __attribute__ ((__unused__)) *L, 
 					const char __attribute__ ((__unused__)) *buf) {return 1;}
 static inline int lua_parser_fn(lua_State __attribute__ ((__unused__)) *L,