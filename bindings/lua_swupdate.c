sbabic/swupdate

commit 3f0ee9b19c207567a5bc3c477da79389c0121aea

lua: drop header from library to avoid license issues
lua_swupdate.so does not use GPLv2 code, but it still includes
lua_util.h that is GPLv2. Drop it because it is unused.

Signed-off-by: Stefano Babic <stefano.babic@swupdate.org>

bindings/lua_swupdate.c
@@ -7,7 +7,7 @@
 
 #include <progress_ipc.h>
 #include <lua.h>
-#include <lua_util.h>
+//#include <lua_util.h>
 #include <lauxlib.h>
 #include <string.h> 
 #include <ifaddrs.h>

corelib/lua_compat.c
@@ -6,7 +6,6 @@
  */
 
 #include <lua.h>
-#include <lua_util.h>
 
 /*
  * These LuaJIT/Lua 5.1 compatibility functions are taken from

commit 28db0bd0663edfaf6ab7faca8bc7e2676bb80134

lua: Remove include line
Instead of commenting out '#include <lua_util.h>', remove it
to make the code cleaner.

Fixes: 3f0ee9b19c20 ("lua: drop header from library to avoid license issues")
Signed-off-by: Fabio Estevam <festevam@gmail.com>

bindings/lua_swupdate.c
@@ -7,7 +7,6 @@
 
 #include <progress_ipc.h>
 #include <lua.h>
-//#include <lua_util.h>
 #include <lauxlib.h>
 #include <string.h> 
 #include <ifaddrs.h>