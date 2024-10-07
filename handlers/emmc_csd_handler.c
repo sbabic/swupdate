eee030139a2e2f801f97019893d16d5b8dfba1c0
Stefano Babic <stefano.babic@swupdate.org>
2024-10-07 17:37:40 +0200

    emmc handler: allow to explicitly set the boot dev
    In some cases it is preferable to set the device to boot instread of
    relying on the toggeling between boot0 and boot1.
    
    The device is set using the full path (/dev/mmcblkXboot[0|1]) in the
    device attribute.
    
    Signed-off-by: Stefano Babic <stefano.babic@swupdate.org>

--- handlers/emmc_csd_handler.c
+++ handlers/emmc_csd_handler.c
@@ -32,37 +32,71 @@
 #include "util.h"
 
 void emmc_csd_handler(void);
+void emmc_csd_toggle_handler(void);
 
-static int emmc_boot_toggle(struct img_type *img, void *data)
+
+/*
+ * This set the CSD register (boot device) on eMMC
+ * The device to be set is passed as argument if toggle is fale
+ * else the booting device is detected and the function toggles
+ * the boot device
+ */
+static int emmc_csd_set(struct img_type *img, void *data, bool toggle)
 {
 	int active, ret;
 	struct script_handler_data *script_data = data;
+	char tmpdev[SWUPDATE_GENERAL_STRING_SIZE];
+
 	if (script_data->scriptfn == PREINSTALL)
 		return 0;
+	
+	strlcpy(tmpdev, img->device, sizeof(tmpdev));
 
-	/* Open the device (partition) */
-	int fdin = open(img->device, O_RDONLY);
+	if (!toggle) {
+		char *boot = strstr(tmpdev, "boot");
+		if (!boot) {
+			ERROR("The boot device as mmcblkXboot[0|1] must be set");
+			return -EINVAL;
+		}
+		if (strlen(boot) < 5) {
+			ERROR("The value for boot device is not set");
+			return -EINVAL;
+		}
+		active = strtoul(&boot[5], NULL, 10);
+		if (errno || (active != 0 && active != 1)) {
+			ERROR("Wrong boot device set: %s", img->device);
+
+		}
+		*boot = '\0';
+	}
+
+	/* Open the device */
+	int fdin = open(tmpdev, O_RDONLY);
 	if (fdin < 0) {
-		ERROR("Failed to open %s: %s", img->device, strerror(errno));
+		ERROR("Failed to open %s: %s", tmpdev, strerror(errno));
 		return -ENODEV;
 	}
 
-	active = emmc_get_active_bootpart(fdin);
-	if (active < 0) {
-		ERROR("Current HW boot partition cannot be retrieved");
-		close(fdin);
-		return -1;
+	if (toggle) {
+		active = emmc_get_active_bootpart(fdin);
+		if (active < 0) {
+			ERROR("Current HW boot partition cannot be retrieved");
+			close(fdin);
+			return -ENODEV;
+		}
+		if (active > 1)
+			WARN("Boot device set to User area, no changes !");
+		else
+			active = (active == 0) ? 1 : 0;
+
 	}
 
 	/*
 	 * If User Partition is activated, does nothing
 	 * and report this to the user.
 	 */
-	if (active > 1) {
-		WARN("Boot device set to User area, no changes !");
-		ret = 0;
-	} else {
-		active = (active == 0) ? 1 : 0;
+	ret = 0;
+	if (active == 0 || active == 1) {
 		TRACE("Setting Boot to HW Partition %d", active);
 		ret = emmc_write_bootpart(fdin, active);
 		if (ret)
@@ -73,9 +107,26 @@ static int emmc_boot_toggle(struct img_type *img, void *data)
 	return ret;
 }
 
+static int emmc_boot(struct img_type *img, void *data)
+{
+	return emmc_csd_set(img, data, false);
+}
+
+static int emmc_boot_toggle(struct img_type *img, void *data)
+{
+	return emmc_csd_set(img, data, true);
+}
+
+__attribute__((constructor))
+void emmc_csd_toggle_handler(void)
+{
+	register_handler("emmc_boot_toggle", emmc_boot_toggle,
+				SCRIPT_HANDLER | NO_DATA_HANDLER, NULL);
+}
+
 __attribute__((constructor))
 void emmc_csd_handler(void)
 {
-	register_handler("emmc_boot_toggle", emmc_boot_toggle,
+	register_handler("emmc_boot", emmc_boot,
 				SCRIPT_HANDLER | NO_DATA_HANDLER, NULL);
 }
