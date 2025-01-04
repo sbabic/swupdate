d6170ebaa68bb42264aa4f0e6e4d64f0ace3c8a6
Daniel Braunwarth <oss@braunwarth.dev>
2025-01-04 22:48:23 +0100

    ci: remove apt cache during setup
    This change optimizes the container layer size.
    
    See https://docs.docker.com/build/building/best-practices.
    
    Signed-off-by: Daniel Braunwarth <oss@braunwarth.dev>

--- ci/setup.sh
+++ ci/setup.sh
@@ -22,57 +22,60 @@ export DEBIAN_FRONTEND=noninteractive
 export TZ=Europe/London
 
 $_SUDO apt-get -qq update && apt-get install --yes --no-install-recommends \
-    cpio \
-    curl \
-    gawk \
-    gcc \
-    git \
-    gulp \
-    libarchive-dev \
-    libblkid-dev \
-    libbtrfsutil-dev \
-    libcmocka-dev \
-    libconfig-dev \
-    libcurl4-openssl-dev \
-    libczmq-dev \
-    libext2fs-dev \
-    libfdisk-dev \
-    libgpiod-dev \
-    libjson-c-dev \
-    liblua5.2-dev \
-    libluajit-5.1-dev \
-    libmbedtls-dev \
-    librsync-dev \
-    libssl-dev \
-    libsystemd-dev \
-    libudev-dev \
-    liburiparser-dev \
-    libwebsockets-dev \
-    libzstd-dev \
-    make \
-    npm \
-    python3 \
-    uuid-dev \
-    zlib1g-dev
+        cpio \
+        curl \
+        gawk \
+        gcc \
+        git \
+        gulp \
+        libarchive-dev \
+        libblkid-dev \
+        libbtrfsutil-dev \
+        libcmocka-dev \
+        libconfig-dev \
+        libcurl4-openssl-dev \
+        libczmq-dev \
+        libext2fs-dev \
+        libfdisk-dev \
+        libgpiod-dev \
+        libjson-c-dev \
+        liblua5.2-dev \
+        libluajit-5.1-dev \
+        libmbedtls-dev \
+        librsync-dev \
+        libssl-dev \
+        libsystemd-dev \
+        libudev-dev \
+        liburiparser-dev \
+        libwebsockets-dev \
+        libzstd-dev \
+        make \
+        npm \
+        python3 \
+        uuid-dev \
+        zlib1g-dev \
+    && rm -rf /var/lib/apt/lists/*
 
 # packages are too old in Ubuntu Jammy
 if ! grep -q UBUNTU_CODENAME=jammy /etc/os-release; then
     $_SUDO apt-get -qq update && apt-get install --yes --no-install-recommends \
-        libebgenv-dev \
-        libmtd-dev \
-        libubi-dev \
-        libubootenv-dev \
-        libzck-dev
+            libebgenv-dev \
+            libmtd-dev \
+            libubi-dev \
+            libubootenv-dev \
+            libzck-dev \
+        && rm -rf /var/lib/apt/lists/*
 else
     $_SUDO apt-get -qq update && apt-get install --yes --no-install-recommends \
-        autoconf \
-        autoconf-archive \
-        automake \
-        check \
-        cmake \
-        liblzo2-dev \
-        libtool \
-        libyaml-dev \
-        meson
+            autoconf \
+            autoconf-archive \
+            automake \
+            check \
+            cmake \
+            liblzo2-dev \
+            libtool \
+            libyaml-dev \
+            meson \
+        && rm -rf /var/lib/apt/lists/*
     "$SCRIPT_DIR/install-src-deps.sh"
 fi
