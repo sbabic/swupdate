sbabic/swupdate

commit 5d57a9c05ec2a4ae422bdb35247a75402f8e0aeb

util: introduce generic mount helpers
Functions asking to mount / umount filesystems must prepare the same
paths for the mountpoints. Factorize this into two new funtions:

	swupdate_temporary_mount()
	swupdate_temporary_umount()

If the mopunt part isn successful, it returns a generated path (not
fixed anymore) as mountpoint. This should be passed to the corresponding
umount(). that becomes responsible to free memory.

Signed-off-by: Stefano Babic <stefano.babic@swupdate.org>

core/util.c
@@ -852,6 +852,69 @@ int swupdate_umount(const char *dir)
 #endif
 }
 
+static const char *mount_points[] = {
+	[MNT_SCRIPTS] = SCRIPTS_DIR_SUFFIX,
+	[MNT_DATA] = DATADST_DIR_SUFFIX,
+	[MNT_BOOT_SCRIPTS] = BOOT_SCRIPT_SUFFIX
+};
+
+char *swupdate_temporary_mount(tmp_mountpoint_t type, const char *device, const char *fstype)
+{
+	char *mountpoint;
+	const char *dir;
+	int ret = 0;
+
+	if (type != MNT_SCRIPTS && type != MNT_DATA && type != MNT_BOOT_SCRIPTS)
+		return NULL;
+
+	if (!device || !strlen(device)) {
+		ERROR("Mount requested without Device");
+		return NULL;
+	}
+
+	dir = mount_points[type];
+	if (asprintf(&mountpoint, "%s%sXXXXXX", get_tmpdir(), dir) == -1) {
+		ERROR("Unable to allocate memory");
+		return NULL;
+	}
+
+	if (!mkdtemp(mountpoint)) {
+		TRACE("Unable to create a unique temporary directory %s: %s",
+			mountpoint, strerror(errno));
+		free(mountpoint);
+		return NULL;
+	}
+
+	ret = swupdate_mount(device, mountpoint, fstype);
+	if (ret) {
+		TRACE("Device %s with filesystem %s cannot be mounted: %s",
+			device, fstype, strerror(errno));
+		rmdir(mountpoint);
+		free(mountpoint);
+		return NULL;
+	}
+
+	return mountpoint;
+}
+
+int swupdate_temporary_umount(char *mountpoint)
+{
+	int ret = 0;
+	if (swupdate_umount(mountpoint)) {
+		TRACE("Unable to unmount %s: %s", mountpoint, strerror(errno));
+		ret = -EFAULT;
+	}
+	if (ret == 0 && rmdir(mountpoint) == -1) {
+		if (errno != EROFS) {
+			TRACE("Unable to remove directory %s: %s", mountpoint, strerror(errno));
+			ret = -EROFS;
+		}
+	}
+	free(mountpoint);
+
+	return ret;
+}
+
 /*
  * Date time in SWUpdate
  * @return : date in ISO8601 (it must be freed by caller)

include/globals.h
@@ -30,3 +30,9 @@
 #define SCRIPTS_DIR_SUFFIX	"scripts/"
 #define DATADST_DIR_SUFFIX	"datadst/"
 #define BOOT_SCRIPT_SUFFIX	"boot-script"
+
+typedef enum {
+	MNT_SCRIPTS,
+	MNT_DATA,
+	MNT_BOOT_SCRIPTS
+} tmp_mountpoint_t;

include/util.h
@@ -290,6 +290,8 @@ int swupdate_remove_directory(const char* path);
 
 int swupdate_mount(const char *device, const char *dir, const char *fstype);
 int swupdate_umount(const char *dir);
+char *swupdate_temporary_mount(tmp_mountpoint_t type, const char *device, const char *fstype);
+int swupdate_temporary_umount(char *mountpoint);
 
 /* Date / Time utilities */
 char *swupdate_time_iso8601(struct timeval *tv);

commit 0c16984c12e496369292a76071738c659bda089c

BUG: util: Append missing slash to temporary mount dir
The raw_handler requires a slash between the mountpoint and the file path.
Otherwise, no correct mount is performed, and the file is saved to the wrong
location.

[TRACE] : SWUPDATE running : [install_raw_file] : Installing file fitimage.itb.signed on /tmp/datadst/UEE6x2fitImage.signed

Fixes: 5d57a9c05ec2 (util: introduce generic mount helpers)
Fixes: aff67cdb2c62 (Make use of introduced swupdate_temporary_(u)mount)
Signed-off-by: Michael Glembotzki <Michael.Glembotzki@iris-sensing.com>
Reviewed-by: Daniel Braunwarth <oss@braunwarth.dev>
Tested-by: Daniel Braunwarth <oss@braunwarth.dev>

core/util.c
@@ -873,6 +873,7 @@ char *swupdate_temporary_mount(tmp_mountpoint_t type, const char *device, const 
 	char *mountpoint;
 	const char *dir;
 	int ret = 0;
+	unsigned int len;
 
 	if (type != MNT_SCRIPTS && type != MNT_DATA && type != MNT_BOOT_SCRIPTS)
 		return NULL;
@@ -883,10 +884,13 @@ char *swupdate_temporary_mount(tmp_mountpoint_t type, const char *device, const 
 	}
 
 	dir = mount_points[type];
-	if (asprintf(&mountpoint, "%s%sXXXXXX", get_tmpdir(), dir) == -1) {
+	len = asprintf(&mountpoint, "%s%sXXXXXX/", get_tmpdir(), dir);
+	if (len == -1) {
 		ERROR("Unable to allocate memory");
 		return NULL;
 	}
+
+	mountpoint[len-1] = '\0'; /* last six characters of template must be XXXXXX */
 
 	if (!mkdtemp(mountpoint)) {
 		TRACE("Unable to create a unique temporary directory %s: %s",
@@ -903,6 +907,8 @@ char *swupdate_temporary_mount(tmp_mountpoint_t type, const char *device, const 
 		free(mountpoint);
 		return NULL;
 	}
+
+	mountpoint[len-1] = '/';  /* restore the trailing slash */
 
 	return mountpoint;
 }