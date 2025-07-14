sbabic/swupdate

commit 566c7471ea1751915fb48d392d931a90eb55bcc8

Support MTD names in copy handler
with this one can do:
  scripts:({
           name="u-boot";
           type = "copy";
           mtdname = "u-boot-b";
           installed-directly = true;
           properties : {
                 copyfrom = "mtd:u-boot-a";
                 type = "preinstall";
                 chain = "raw";
           }
  });

Signed-off-by: Joakim Tjernlund <joakim.tjernlund@infinera.com>

handlers/copy_handler.c
@@ -44,6 +44,7 @@
 #include "progress.h"
 #include "handler.h"
 #include "util.h"
+#include "flash.h"
 #include "handler_helpers.h"
 #include "installer.h"
 
@@ -235,7 +236,15 @@ static int copy_image_file(struct img_type *img, void *data)
 	script_data = data;
 
 	base_img = img;
-
+	if(strlen(img->mtdname) ){
+		int mtdnum = get_mtd_from_name(img->mtdname);
+		if (mtdnum < 0) {
+			ERROR("Wrong MTD name in description: %s",
+			      img->mtdname);
+			return -1;
+		}
+		snprintf(img->device, sizeof(img->device), "/dev/mtdblock%d", mtdnum);
+	}
 	proplist = dict_get_list(&img->properties, "type");
 	if (proplist) {
 		entry = LIST_FIRST(proplist);
@@ -258,7 +267,17 @@ static int copy_image_file(struct img_type *img, void *data)
 		ERROR("Missing source device, no copyfrom property");
 		return -EINVAL;
 	}
-
+	if (!strncmp("mtd:", entry->value, 4)) {
+		int mtdnum = get_mtd_from_name(entry->value + 4);
+		if (mtdnum < 0) {
+			ERROR("Wrong MTD name in copyfrom: %s",
+			      entry->value);
+			return -1;
+		}
+		free(entry->value);
+		entry->value = malloc(20+1); /* to hold "/dev/mtdblockXX */
+		snprintf(entry->value, 20, "/dev/mtdblock%d", mtdnum);
+	}
 	copyfrom = realpath(entry->value, NULL);
 	if (!copyfrom) {
 		ERROR("%s cannot be resolved", entry->value);

commit 74a3f87307f062a0fed6018b5d43159dbc45f328

handlers: copy_handler: Fix !CONFIG_MTD build
Commit 566c7471 broke builds with !CONFIG_MTD,
fix it with according #ifdef CONFIG_MTD.

Signed-off-by: Christian Storm <christian.storm@siemens.com>

handlers/copy_handler.c
@@ -44,7 +44,9 @@
 #include "progress.h"
 #include "handler.h"
 #include "util.h"
+#ifdef CONFIG_MTD
 #include "flash.h"
+#endif
 #include "handler_helpers.h"
 #include "installer.h"
 
@@ -236,6 +238,7 @@ static int copy_image_file(struct img_type *img, void *data)
 	script_data = data;
 
 	base_img = img;
+#ifdef CONFIG_MTD
 	if(strlen(img->mtdname) ){
 		int mtdnum = get_mtd_from_name(img->mtdname);
 		if (mtdnum < 0) {
@@ -245,6 +248,7 @@ static int copy_image_file(struct img_type *img, void *data)
 		}
 		snprintf(img->device, sizeof(img->device), "/dev/mtdblock%d", mtdnum);
 	}
+#endif
 	proplist = dict_get_list(&img->properties, "type");
 	if (proplist) {
 		entry = LIST_FIRST(proplist);
@@ -267,6 +271,7 @@ static int copy_image_file(struct img_type *img, void *data)
 		ERROR("Missing source device, no copyfrom property");
 		return -EINVAL;
 	}
+#ifdef CONFIG_MTD
 	if (!strncmp("mtd:", entry->value, 4)) {
 		int mtdnum = get_mtd_from_name(entry->value + 4);
 		if (mtdnum < 0) {
@@ -278,6 +283,7 @@ static int copy_image_file(struct img_type *img, void *data)
 		entry->value = malloc(20+1); /* to hold "/dev/mtdblockXX */
 		snprintf(entry->value, 20, "/dev/mtdblock%d", mtdnum);
 	}
+#endif
 	copyfrom = realpath(entry->value, NULL);
 	if (!copyfrom) {
 		ERROR("%s cannot be resolved", entry->value);