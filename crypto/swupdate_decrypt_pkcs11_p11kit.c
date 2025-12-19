88502491f66ae0e6c23eef072d40bfcaef508a99
Matej Zachar <zachar.matej@gmail.com>
2025-12-19 12:21:57 +0100

    Increased PKCS#11 decryption performance with p11-kit
    On platforms using "opTee + pkcs11 TA + RPMB" the decryption
    speed is increased by order of magnitude - as the C_DecryptInit
    context is not lost between decryption updates (C_DecryptUpdate).
    On my board (iMX8MP) it went from 50min with wolfSSL to 3min with p11-kit
    
    Signed-off-by: Matej Zachar <zachar.matej@gmail.com>
    Reviewed-by: Stefano Babic <stefano.babic@swupdate.org>

---
+++ crypto/swupdate_decrypt_pkcs11_p11kit.c
@@ -0,0 +1,290 @@
+// SPDX-FileCopyrightText: 2024 Matej Zachar
+//
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Inspired by the wolfssl implementation done by Bastian Germann
+ */
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "swupdate_crypto.h"
+#include "swupdate_pkcs11.h"
+#include "util.h"
+
+static CK_SLOT_ID find_slot(CK_FUNCTION_LIST_PTR module, P11KitUri *uri)
+{
+	CK_RV rv;
+
+	CK_SLOT_ID slot_id = p11_kit_uri_get_slot_id(uri);
+	if (slot_id != (CK_SLOT_ID)-1)
+		return slot_id;
+
+	size_t slot_count;
+	rv = module->C_GetSlotList(1, NULL_PTR, &slot_count);
+	if (rv != CKR_OK)
+		return (CK_SLOT_ID)-1;
+
+	CK_SLOT_ID slot_ids[slot_count];
+	rv = module->C_GetSlotList(1, &slot_ids[0], &slot_count);
+	if (rv != CKR_OK)
+		return (CK_SLOT_ID)-1;
+
+	CK_TOKEN_INFO token_info;
+	for (int i = 0; i < slot_count; ++i) {
+		slot_id = slot_ids[i];
+
+		rv = module->C_GetTokenInfo(slot_id, &token_info);
+		if (rv != CKR_OK)
+			return (CK_SLOT_ID)-1;
+
+		if (p11_kit_uri_match_token_info(uri, &token_info))
+			return slot_id;
+	}
+
+	return (CK_SLOT_ID)-1;
+}
+
+static CK_RV find_key(CK_FUNCTION_LIST_PTR module, CK_SESSION_HANDLE session,
+	CK_ATTRIBUTE_PTR key_id, CK_OBJECT_HANDLE *key_handle)
+{
+	CK_RV rv;
+
+	CK_ATTRIBUTE find_template[] = {
+		{ CKA_ID, key_id->pValue, key_id->ulValueLen }
+	};
+
+	rv = module->C_FindObjectsInit(session, find_template, 1);
+	if (rv != CKR_OK) {
+		return rv;
+	}
+
+	CK_ULONG object_count;
+	rv = module->C_FindObjects(session, key_handle, 1, &object_count);
+	if (rv != CKR_OK) {
+		return rv;
+	}
+
+	rv = module->C_FindObjectsFinal(session);
+	if (rv != CKR_OK) {
+		return rv;
+	}
+
+	if (object_count == 0) {
+		return CKR_DATA_INVALID;
+	}
+
+	return CKR_OK;
+}
+
+struct pkcs11_digest *pkcs11_DECRYPT_init(unsigned char *uri,
+	char __attribute__ ((__unused__)) keylen, unsigned char *iv, cipher_t cipher)
+{
+	struct pkcs11_digest *dgst;
+	CK_SLOT_ID slot_id;
+	CK_ATTRIBUTE_PTR key_id;
+	const char *pin;
+	const char *module_path;
+	const char *msg;
+	int err = 0;
+	CK_RV rv;
+
+	if (uri == NULL || iv == NULL) {
+		ERROR("PKCS#11 URI or AES IV missing for decryption!");
+		return NULL;
+	}
+
+	dgst = calloc(1, sizeof(*dgst));
+	if (!dgst) {
+		return NULL;
+	}
+
+	dgst->uri = p11_kit_uri_new();
+	err = p11_kit_uri_parse((const char*)uri, P11_KIT_URI_FOR_OBJECT_ON_TOKEN_AND_MODULE, dgst->uri);
+	if (err) {
+		msg = p11_kit_uri_message(err);
+		ERROR("PKCS#11 URI: %s", msg);
+		goto free_digest;
+	}
+
+	key_id	= p11_kit_uri_get_attribute(dgst->uri, CKA_ID);
+	pin	= p11_kit_uri_get_pin_value(dgst->uri);
+	module_path = p11_kit_uri_get_module_path(dgst->uri);
+	if (key_id == NULL || pin == NULL || module_path == NULL) {
+		ERROR("PKCS#11 URI must contain id, pin-value and module-path.");
+		goto free_digest;
+	}
+
+	dgst->module = p11_kit_module_load(module_path, 0);
+	if (dgst->module == NULL) {
+		msg = p11_kit_message();
+		ERROR("Failed to load PKCS#11 module [%s]: %s\n", module_path, msg);
+		goto free_digest;
+	}
+
+	rv = dgst->module->C_Initialize(NULL_PTR);
+	if (rv != CKR_OK)
+		goto err_msg;
+
+	slot_id = find_slot(dgst->module, dgst->uri);
+	if (slot_id == -1) {
+		ERROR("PKCS#11 URI must contain slot-id or token identification such as token, model, serial, manufacturer.");
+		goto free_digest;
+	}
+
+	rv = dgst->module->C_OpenSession(slot_id, CKF_SERIAL_SESSION | CKF_RW_SESSION, NULL_PTR, NULL_PTR, &dgst->session);
+	if (rv != CKR_OK)
+		goto err_msg;
+
+	rv = dgst->module->C_Login(dgst->session, CKU_USER, (unsigned char *)pin, strnlen(pin, 32));
+	if (rv != CKR_OK)
+		goto err_msg;
+
+	CK_OBJECT_HANDLE key;
+	rv = find_key(dgst->module, dgst->session, key_id, &key);
+	if (rv != CKR_OK)
+		goto err_msg;
+
+	// Setup a valid PKCS#7 block plus one state octet
+	for (int i = 0; i <= AES_BLK_SIZE; ++i) {
+		dgst->last[i] = AES_BLK_SIZE;
+	}
+
+	// Setup IV vector & mechanism
+	memcpy(dgst->iv, iv, AES_BLK_SIZE);
+	dgst->mechanism.mechanism = CKM_AES_CBC;
+	dgst->mechanism.pParameter = dgst->iv;
+	dgst->mechanism.ulParameterLen = AES_BLK_SIZE;
+
+	rv = dgst->module->C_DecryptInit(dgst->session, &dgst->mechanism, key);
+	if (rv != CKR_OK)
+		goto err_msg;
+
+	INFO("PKCS#11 key set up successfully.");
+	return dgst;
+
+err_msg:
+	msg = p11_kit_strerror(rv);
+	ERROR("PKCS#11 initialization failed: %s", msg);
+
+free_digest:
+	if (dgst->uri)
+		p11_kit_uri_free(dgst->uri);
+
+	if (dgst->session)
+		dgst->module->C_CloseSession(dgst->session);
+
+	if (dgst->module) {
+		dgst->module->C_Finalize(NULL_PTR);
+		p11_kit_module_release(dgst->module);
+	}
+
+	free(dgst);
+
+	return NULL;
+}
+
+int pkcs11_DECRYPT_update(struct pkcs11_digest *dgst, unsigned char *buf,
+	int *outlen, const unsigned char *cryptbuf, int inlen)
+{
+	// precondition: len(buf) >= inlen + AES_BLK_SIZE
+	unsigned long buf_len = inlen + AES_BLK_SIZE;
+	CK_RV rv;
+
+	if (inlen < AES_BLK_SIZE)
+		return -EFAULT;
+
+	if (dgst->last[AES_BLK_SIZE]) {
+		dgst->last[AES_BLK_SIZE] = 0;
+		// first run - there is no block to append
+		*outlen = 0;
+	} else {
+		// append previously decrypted last AES block
+		memcpy(buf, dgst->last, AES_BLK_SIZE);
+		buf += AES_BLK_SIZE;
+		*outlen = AES_BLK_SIZE;
+	}
+
+	rv = dgst->module->C_DecryptUpdate(dgst->session, (unsigned char*)cryptbuf, inlen, buf, &buf_len);
+	if (rv != CKR_OK) {
+		ERROR("PKCS#11 AES decryption failed: %s", p11_kit_strerror(rv));
+		return -EFAULT;
+	}
+
+	// strip and remember last AES block from decoded buffer
+	// it will get appended either in the next call to DECRYPT_update or DECRYPT_final
+	buf_len -= AES_BLK_SIZE;
+	memcpy(dgst->last, &buf[buf_len], AES_BLK_SIZE);
+
+	// update iv for the next block
+	memcpy(dgst->iv, cryptbuf + inlen - AES_BLK_SIZE, AES_BLK_SIZE);
+
+	*outlen += (int)buf_len;
+	return 0;
+}
+
+int pkcs11_DECRYPT_final(struct pkcs11_digest *dgst, unsigned char *buf, int *outlen)
+{
+	CK_RV rv;
+	unsigned long extra_len = 0;
+
+	if (dgst->last[AES_BLK_SIZE]) {
+#ifndef CONFIG_ENCRYPTED_IMAGES_HARDEN_LOGGING
+		ERROR("AES: At least one call to pkcs11_DECRYPT_update was expected");
+#endif
+		return -EINVAL;
+	}
+
+	// append previously decrypted last AES block if any
+	memcpy(buf, dgst->last, AES_BLK_SIZE);
+
+	rv = dgst->module->C_DecryptFinal(dgst->session, &buf[AES_BLK_SIZE], &extra_len);
+	if (rv != CKR_OK)
+		return -EFAULT;
+
+	// obtain last AES block after C_DecryptFinal
+	CK_BYTE_PTR last = &buf[extra_len];
+
+	// Handle manual PKCS#7 padding removal
+	CK_BYTE padding_value = last[AES_BLK_SIZE - 1];
+
+	if (padding_value <= 0 || padding_value > AES_BLK_SIZE) {
+#ifndef CONFIG_ENCRYPTED_IMAGES_HARDEN_LOGGING
+		ERROR("AES: Invalid PKCS#7 padding value [%u]", padding_value);
+#endif
+		return -EFAULT;
+	}
+
+	// Verify that padding is correct
+	for (CK_BYTE i = 0; i < padding_value; ++i) {
+		if (last[AES_BLK_SIZE - 1 - i] != padding_value) {
+#ifndef CONFIG_ENCRYPTED_IMAGES_HARDEN_LOGGING
+			ERROR("AES: Invalid PKCS#7 padding value [%u] at offset %u", padding_value, i);
+#endif
+			return -EINVAL;
+		}
+	}
+
+	*outlen = (int)extra_len + AES_BLK_SIZE - padding_value;
+	return 0;
+}
+
+void pkcs11_DECRYPT_cleanup(struct pkcs11_digest *dgst)
+{
+	if (dgst) {
+		if (dgst->uri)
+			p11_kit_uri_free(dgst->uri);
+
+		if (dgst->session)
+			dgst->module->C_CloseSession(dgst->session);
+
+		if (dgst->module) {
+			dgst->module->C_Finalize(NULL_PTR);
+			p11_kit_module_release(dgst->module);
+		}
+
+		free(dgst);
+		dgst = NULL;
+	}
+}
---
+++ test/test_crypt_pkcs11.c
@@ -0,0 +1,98 @@
+// SPDX-FileCopyrightText: 2024 Matej Zachar
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <util.h>
+#include "swupdate_crypto.h"
+
+#define BUFFER_SIZE (AES_BLK_SIZE * 1024)
+#define TOKENDIR "test/data/token"
+
+static int read_file(const char *path, unsigned char *buffer, size_t *size)
+{
+	FILE *fp = fopen(path, "r");
+	if (!fp) {
+		fprintf(stderr, "Failed to open file '%s'\n", path);
+		return -1;
+	}
+
+	size_t len = fread(buffer, sizeof(char), *size, fp);
+	if (ferror(fp) != 0) {
+		fprintf(stderr, "Error reading file '%s'\n", path);
+		fclose(fp);
+		return -1;
+	}
+
+	*size = len;
+	fclose(fp);
+
+	return 0;
+}
+
+static void test_crypt_pkcs11_256(void **state)
+{
+	(void) state;
+	int err;
+
+	const char * uri = "pkcs11:token=TestToken;id=%A1%B2?pin-value=1234&module-path=/usr/lib/softhsm/libsofthsm2.so";
+
+	size_t original_data_len = 128 * 1024;/* 128KiB */
+	unsigned char original_data[original_data_len];
+	err = read_file(TOKENDIR "/original.data", &original_data[0], &original_data_len);
+	assert_true(err == 0);
+
+	size_t encrypted_data_len = 128 * 1024 + AES_BLK_SIZE;/* 128KiB AES_BLK_SIZE(16B) */
+	unsigned char encrypted_data[encrypted_data_len];
+	err = read_file(TOKENDIR "/encrypted.data", &encrypted_data[0], &encrypted_data_len);
+	assert_true(err == 0);
+
+	unsigned char decrypted_data[encrypted_data_len];
+
+	size_t iv_size = 16;
+	unsigned char iv[iv_size];
+	err = read_file(TOKENDIR "/encrypted.data.iv", &iv[0], &iv_size);
+	assert_true(err == 0);
+
+	unsigned char buffer[BUFFER_SIZE + AES_BLK_SIZE];
+
+	struct swupdate_digest *dgst = swupdate_DECRYPT_init((unsigned char *)uri, 0, &iv[0], AES_CBC_256);
+	assert_non_null(dgst);
+
+	int len;
+	size_t e_offset = 0;
+	size_t d_offset = 0;
+	while (e_offset < encrypted_data_len) {
+		size_t chunk_size = (encrypted_data_len - e_offset > BUFFER_SIZE) ? BUFFER_SIZE : encrypted_data_len - e_offset;
+
+		err = swupdate_DECRYPT_update(dgst, buffer, &len, encrypted_data + e_offset, chunk_size);
+		assert_true(err == 0);
+		assert_true(len >= AES_BLK_SIZE && len <= chunk_size);
+		e_offset += chunk_size;
+
+		memcpy(&decrypted_data[d_offset], buffer, len);
+		d_offset += len;
+	}
+
+	err = swupdate_DECRYPT_final(dgst, buffer, &len);
+	assert_true(err == 0);
+	assert_true(len == 3); /* as the size is 128*1024+3 */
+
+	memcpy(&decrypted_data[d_offset], buffer, len);
+	d_offset += len;
+
+	assert_true(strncmp((const char *)decrypted_data, (const char *)original_data, original_data_len) == 0);
+}
+
+int main(void)
+{
+	const struct CMUnitTest crypt_pkcs11_tests[] = {
+		cmocka_unit_test(test_crypt_pkcs11_256)
+	};
+	return cmocka_run_group_tests_name("crypt_pkcs11", crypt_pkcs11_tests, NULL, NULL);
+}
