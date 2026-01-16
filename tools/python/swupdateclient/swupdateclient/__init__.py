03099af2148c6f47e1d1f55907353b75346539ed
Stefano Babic <stefano.babic@swupdate.org>
2026-01-16 09:45:43 +0100

    Avoid that empty __init__.py are removed in cleanup
    distclean removes all files with size 0. swupdateclient/__init__.py is
    more a placeholder but it shouldn't be removed.
    
    Signed-off-by: Stefano Babic <stefano.babic@swupdate.org>

---
+++ tools/python/swupdateclient/swupdateclient/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2016 Stefano Babic <stefano.babic@swupdate.org>
+#
+# SPDX-License-Identifier:     GPL-2.0-only
+#
+# This is dummy here
+# it is just to avoid that the file has size 0 and could be
+# removed during cleanup
