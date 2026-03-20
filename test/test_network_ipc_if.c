sbabic/swupdate

commit 89ee5291ba3c44237e169e1e1b59e065873f420f

Add IPC to send a list of URL as sources
This is used together with delta update to get where the ZCLK file is
stored. In many case, as with Hawkbit, the URL is not known at build
time and cannot be stored into sw-description. Hawkbit generates a URL
when the file is uploaded, and sends this URL back in the list of
Software Modules. SWUpdate had ignored this as it is not a SWU file.

By adding rules for files in Hawkbiot connector, suricatta sends the
list of received URLs to the core, anbd this can be used later by a
handler (delta) when it runs.

Signed-off-by: Stefano Babic <stefano.babic@swupdate.org>

ipc/network_ipc-if.c
@@ -405,6 +405,23 @@ int swupdate_set_version_range(const char *minversion,
 						      NULL);
 }
 
+int swupdate_dwl_url (const char *artifact_name, const char *url)
+{
+	ipc_message msg;
+
+	memset(&msg, 0, sizeof(msg));
+	msg.magic = IPC_MAGIC;
+	msg.type = SET_DELTA_URL;
+
+	strlcpy(msg.data.dwl_url.filename,
+			artifact_name,
+			sizeof(msg.data.dwl_url.filename) - 1);
+	strlcpy(msg.data.dwl_url.url,
+			url,
+			sizeof(msg.data.dwl_url.url) - 1);
+	return ipc_send_cmd(&msg);
+}
+
 void swupdate_prepare_req(struct swupdate_request *req) {
 	if (!req)
 		return;

core/installer.c
@@ -558,6 +558,11 @@ void cleanup_files(struct swupdate_cfg *software) {
 	dict_drop_db(&software->vars);
 
 	/*
+	 * Drop per update URLs
+	 */
+	dict_drop_db(&software->external_urls);
+
+	/*
 	 * Drop Lua State if instantiated
 	 */
 	if (software->lua_state) {

core/network_thread.c
@@ -629,6 +629,23 @@ void *network_thread (void *data)
 				} else
 					msg.type = NACK;
 				break;
+			case SET_DELTA_URL:
+				cfg = get_swupdate_cfg();
+
+				/*
+				 * check strings in IPC
+				 */
+				msg.data.dwl_url.filename[sizeof(msg.data.dwl_url.filename) - 1] = '\0';
+				msg.data.dwl_url.url[sizeof(msg.data.dwl_url.url) - 1] = '\0';
+				/*
+				 * Check for mutex: there is currently no other users, one writer and one reader
+				 * for the list (the handler).
+				 */
+				TRACE("EXTERNAL URL: %s:%s",msg.data.dwl_url.filename, msg.data.dwl_url.url );
+				msg.type = dict_insert_value(&cfg->external_urls,
+							     msg.data.dwl_url.filename,
+							     msg.data.dwl_url.url) == 0 ? ACK : NACK;
+				break;
 			default:
 				msg.type = NACK;
 			}

include/network_ipc.h
@@ -40,6 +40,7 @@ typedef enum {
 	GET_HW_REVISION,
 	SET_SWUPDATE_VARS,
 	GET_SWUPDATE_VARS,
+	SET_DELTA_URL,
 } msgtype;
 
 /*
@@ -127,6 +128,10 @@ typedef union {
 		char varname[256];
 		char varvalue[256];
 	} vars;
+	struct {
+		char filename[256];
+		char url[1024];
+	} dwl_url;
 } msgdata;
 	
 typedef struct {
@@ -164,6 +169,8 @@ int swupdate_set_version_range_type(const char *updatetype,
 				const char *minversion,
 				const char *maxversion,
 				const char *currentversion);
+int swupdate_dwl_url (const char *artifact_name, const char *url);
+
 #ifdef __cplusplus
 }   // extern "C"
 #endif

include/swupdate.h
@@ -104,6 +104,7 @@ struct swupdate_cfg {
 	struct dict bootloader;
 	struct dict vars;
 	struct dict accepted_set;
+	struct dict external_urls;
 	struct proclist extprocs;
 	void *dgst;	/* Structure for signed images */
 	struct swupdate_parms parms;

suricatta/server_hawkbit.c
@@ -49,8 +49,8 @@ static bool swu_rule(artifact_t __attribute__ ((__unused__)) *artifact) {
 	return true;
 }
 static bool zck_rule(artifact_t *artifact) {
-	artifact = artifact;
-	return true;
+	artifact->skip = true;
+	return swupdate_dwl_url(artifact->filename, artifact->url);
 }
 static bool reject_rule(artifact_t __attribute__ ((__unused__)) *artifact) {
 	return false;

commit e3b3c977e200283c4eaacb7aa70b28f0cfbde704

ipc: remove strlcpy dependency from swupdate_dwl_url
swupdate_dwl_url() is part of libswupdate, but SET_DELTA_URL support
started using strlcpy() there. The helper is provided by core/strlcpy.o,
which is not linked into libswupdate, so configurations that build the
shared IPC client library fail with an undefined reference.

Replace it with the same memset plus strncpy pattern already used by the
other IPC helper setters, and add a focused regression test for
swupdate_dwl_url().

Fixes: 89ee5291ba3c ("Add IPC to send a list of URL as sources")
Signed-off-by: Aviv Daum <aviv.daum@gmail.com>

ipc/network_ipc-if.c
@@ -413,12 +413,13 @@ int swupdate_dwl_url (const char *artifact_name, const char *url)
 	msg.magic = IPC_MAGIC;
 	msg.type = SET_DELTA_URL;
 
-	strlcpy(msg.data.dwl_url.filename,
-			artifact_name,
-			sizeof(msg.data.dwl_url.filename) - 1);
-	strlcpy(msg.data.dwl_url.url,
-			url,
-			sizeof(msg.data.dwl_url.url) - 1);
+	strncpy(msg.data.dwl_url.filename,
+		artifact_name,
+		sizeof(msg.data.dwl_url.filename) - 1);
+	strncpy(msg.data.dwl_url.url,
+		url,
+		sizeof(msg.data.dwl_url.url) - 1);
+
 	return ipc_send_cmd(&msg);
 }
 

test/Makefile
@@ -29,7 +29,10 @@ tests-$(CONFIG_SURICATTA_HAWKBIT) += test_json
 tests-$(CONFIG_SURICATTA_HAWKBIT) += test_server_hawkbit
 tests-$(CONFIG_MONGOOSE) += test_mongoose_upload
 tests-y += test_util
+tests-y += test_network_ipc_if
 tests-$(CONFIG_CFI) += test_flash_handler
+
+test_network_ipc_if-extra-objs := $(objtree)/ipc/network_ipc-if.o
 
 ccflags-y += -I$(src)/../
 
@@ -64,7 +67,7 @@ quiet_cmd_linktestexe = LD      $(basename $@)
 						"$(CC)" \
 						"$(KBUILD_CFLAGS)" \
 						"$(LDFLAGS) $(EXTRA_LDFLAGS) $(LDMOCKS)" \
-						"$(basename $@).o $(subst core/built-in.o,core/built-in.o.tmp,$(SWOBJS))" \
+						"$(basename $@).o $($(notdir $(basename $@))-extra-objs) $(subst core/built-in.o,core/built-in.o.tmp,$(SWOBJS))" \
 						"$(SWLIBS)" \
 						"$(LDLIBS) cmocka"
 
@@ -86,6 +89,8 @@ endif
 
 $(objtree)/core/built-in.o.tmp: $(objtree)/core/built-in.o
 	$(Q)$(STRIP) -N main -o $(objtree)/core/built-in.o.tmp $(objtree)/core/built-in.o
+
+$(obj)/test_network_ipc_if.lnk: $(test_network_ipc_if-extra-objs)
 
 $(obj)/%.lnk: $(obj)/%.o $(objtree)/core/built-in.o.tmp
 	$(Q)$(call cmd,linktestexe)

test/test_network_ipc_if.c
@@ -1,0 +1,71 @@
+// SPDX-FileCopyrightText: 2026 Aviv Daum <aviv.daum@gmail.com>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <cmocka.h>
+
+#include "network_ipc.h"
+
+static ipc_message last_msg;
+
+extern int __real_ipc_send_cmd(ipc_message *msg);
+int __wrap_ipc_send_cmd(ipc_message *msg);
+int __wrap_ipc_send_cmd(ipc_message *msg)
+{
+	last_msg = *msg;
+	return 0;
+}
+
+static int network_ipc_if_setup(void **state)
+{
+	(void)state;
+	memset(&last_msg, 0, sizeof(last_msg));
+	return 0;
+}
+
+static void test_swupdate_dwl_url_populates_message(void **state)
+{
+	(void)state;
+
+	assert_int_equal(swupdate_dwl_url("artifact.swu", "https://example.invalid/a.swu"), 0);
+	assert_int_equal(last_msg.magic, IPC_MAGIC);
+	assert_int_equal(last_msg.type, SET_DELTA_URL);
+	assert_string_equal(last_msg.data.dwl_url.filename, "artifact.swu");
+	assert_string_equal(last_msg.data.dwl_url.url, "https://example.invalid/a.swu");
+}
+
+static void test_swupdate_dwl_url_truncates_and_terminates(void **state)
+{
+	char filename[400];
+	char url[1200];
+
+	(void)state;
+
+	memset(filename, 'f', sizeof(filename) - 1);
+	filename[sizeof(filename) - 1] = '\0';
+	memset(url, 'u', sizeof(url) - 1);
+	url[sizeof(url) - 1] = '\0';
+
+	assert_int_equal(swupdate_dwl_url(filename, url), 0);
+	assert_int_equal(last_msg.data.dwl_url.filename[sizeof(last_msg.data.dwl_url.filename) - 1], '\0');
+	assert_int_equal(last_msg.data.dwl_url.url[sizeof(last_msg.data.dwl_url.url) - 1], '\0');
+	assert_int_equal(strlen(last_msg.data.dwl_url.filename), sizeof(last_msg.data.dwl_url.filename) - 1);
+	assert_int_equal(strlen(last_msg.data.dwl_url.url), sizeof(last_msg.data.dwl_url.url) - 1);
+}
+
+int main(void)
+{
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(test_swupdate_dwl_url_populates_message),
+		cmocka_unit_test(test_swupdate_dwl_url_truncates_and_terminates),
+	};
+
+	return cmocka_run_group_tests_name("network_ipc_if", tests,
+					   network_ipc_if_setup, NULL);
+}