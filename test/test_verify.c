61e3cb001a176c9aae90c407dfe7ef516ad0219b
Daniel Braunwarth <daniel.braunwarth@kuka.com>
2026-07-02 12:14:30 +0200

    test: add CMS CRL verification tests for OpenSSL
    Add CMS verification test coverage for OpenSSL CRL handling, including:
    verification without CRL, revoked signer with PEM CRL, revoked signer
    with DER CRL, and a non-revoked signer with an empty CRL.
    
    Extend test fixture generation in test/Makefile to create CMS signature
    artifacts and CRLs, and add test/data/.gitignore entries for generated
    fixture files.
    
    Co-developed-by: Franz Heger <franz.heger@kuka.com>
    Signed-off-by: Franz Heger <franz.heger@kuka.com>
    Signed-off-by: Daniel Braunwarth <daniel.braunwarth@kuka.com>
    Reviewed-by: Stefano Babic <stefano.babic@swupdate.org>

--- test/test_verify.c
+++ test/test_verify.c
@@ -21,6 +21,7 @@
 #include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <string.h>
 #include <cmocka.h>
 
 #include "swupdate_crypto.h"
@@ -44,11 +45,86 @@ static void test_verify_pkcs15(void **state)
 	assert_int_equal(error, 0);
 }
 
+#if defined(CONFIG_SIGALG_CMS) && defined(CONFIG_SSL_IMPL_OPENSSL)
+static void test_verify_cms_without_crl(void **state)
+{
+	int error;
+	struct swupdate_cfg config;
+
+	(void)state;
+
+	memset(&config, 0, sizeof(config));
+	error = swupdate_dgst_init(&config, DATADIR "cms-ca.cert.pem");
+	assert_int_equal(error, 0);
+
+	error = swupdate_verify_file(config.dgst, DATADIR "signature.cms",
+		DATADIR "to-be-signed", NULL);
+	assert_int_equal(error, 0);
+}
+
+static void test_verify_cms_with_revoked_signer_crl(void **state)
+{
+	int error;
+	struct swupdate_cfg config;
+
+	(void)state;
+
+	memset(&config, 0, sizeof(config));
+	strlcpy(config.crlfname, DATADIR "cms-ca.crl.pem", sizeof(config.crlfname));
+	error = swupdate_dgst_init(&config, DATADIR "cms-ca.cert.pem");
+	assert_int_equal(error, 0);
+
+	error = swupdate_verify_file(config.dgst, DATADIR "signature.cms",
+		DATADIR "to-be-signed", NULL);
+	assert_int_not_equal(error, 0);
+}
+
+static void test_verify_cms_with_revoked_signer_der_crl(void **state)
+{
+	int error;
+	struct swupdate_cfg config;
+
+	(void)state;
+
+	memset(&config, 0, sizeof(config));
+	strlcpy(config.crlfname, DATADIR "cms-ca.crl.der", sizeof(config.crlfname));
+	error = swupdate_dgst_init(&config, DATADIR "cms-ca.cert.pem");
+	assert_int_equal(error, 0);
+
+	error = swupdate_verify_file(config.dgst, DATADIR "signature.cms",
+		DATADIR "to-be-signed", NULL);
+	assert_int_not_equal(error, 0);
+}
+
+static void test_verify_cms_with_nonrevoked_crl(void **state)
+{
+	int error;
+	struct swupdate_cfg config;
+
+	(void)state;
+
+	memset(&config, 0, sizeof(config));
+	strlcpy(config.crlfname, DATADIR "cms-ca.crl.empty.pem", sizeof(config.crlfname));
+	error = swupdate_dgst_init(&config, DATADIR "cms-ca.cert.pem");
+	assert_int_equal(error, 0);
+
+	error = swupdate_verify_file(config.dgst, DATADIR "signature.cms",
+		DATADIR "to-be-signed", NULL);
+	assert_int_equal(error, 0);
+}
+#endif
+
 int main(void)
 {
 	swupdate_crypto_init();
 	static const struct CMUnitTest verify_tests[] = {
 		cmocka_unit_test(test_verify_pkcs15),
+#if defined(CONFIG_SIGALG_CMS) && defined(CONFIG_SSL_IMPL_OPENSSL)
+		cmocka_unit_test(test_verify_cms_without_crl),
+		cmocka_unit_test(test_verify_cms_with_revoked_signer_crl),
+		cmocka_unit_test(test_verify_cms_with_revoked_signer_der_crl),
+		cmocka_unit_test(test_verify_cms_with_nonrevoked_crl),
+#endif
 	};
 	return cmocka_run_group_tests_name("verify", verify_tests, NULL, NULL);
 }
