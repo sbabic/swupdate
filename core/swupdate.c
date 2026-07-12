fa08d389c28d10de20242c114a97624cf6dba5df
Stefano Babic <stefano.babic@swupdate.org>
2026-07-12 13:57:39 +0200

    Remove warning : discards ‘const’ in swupdate.c
    core/swupdate.c:268:13: warning: assignment discards ‘const’ qualifier from pointer target type
      268 |         pos = strchr(selector, ',');
          |             ^
    
    Signed-off-by: Stefano Babic <stefano.babic@swupdate.org>

--- core/swupdate.c
+++ core/swupdate.c
@@ -265,7 +265,7 @@ static int parse_image_selector(const char *selector, struct swupdate_cfg *sw)
 	char *pos;
 
 	DEBUG("Parsing selector: %s", selector);
-	pos = strchr(selector, ',');
+	pos = (char *)strchr(selector, ',');
 	if (pos == NULL) {
 		ERROR("Incorrect select option format: %s", selector);
 		return -EINVAL;
